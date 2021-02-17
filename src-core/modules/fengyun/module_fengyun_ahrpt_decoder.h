65a96d123d58a4a4901d1e0d2446ee83a48a2c12
Aang23 <qwerty15@gmx.fr>
2021-02-17 23:38:15 +0100

    Same fix in the FY AHRPT dec

--- src-core/modules/fengyun/module_fengyun_ahrpt_decoder.h
+++ src-core/modules/fengyun/module_fengyun_ahrpt_decoder.h
@@ -44,7 +44,8 @@ namespace fengyun
 
         std::ifstream data_in;
         std::ofstream data_out;
-        size_t filesize;
+        std::atomic<size_t> filesize;
+        std::atomic<size_t> progress;
 
         FengyunAHRPTViterbi viterbi1, viterbi2;
         CADUDeframer deframer;
