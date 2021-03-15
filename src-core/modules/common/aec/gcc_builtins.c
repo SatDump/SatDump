4865b2466fb3a8385d6637e58a8f71c56e133e4c
Aang23 <qwerty15@gmx.fr>
2021-03-15 15:54:19 +0100

    Hopefully fix libaec on MSVC?

--- src-core/modules/common/aec/gcc_builtins.c
+++ src-core/modules/common/aec/gcc_builtins.c
@@ -2,28 +2,49 @@
     MSVC lacks some builtin functions GCC has
 */
 #ifdef _MSC_VER
-#include <stdint.h>
+#include <intrin.h>
 
-uint32_t __builtin_clz(const uint64_t x)
-{
-    uint32_t u32 = (x >> 32);
-    uint32_t result = u32 ? __builtin_clz(u32) : 32;
-    if (result == 32)
-    {
-        u32 = x & 0xFFFFFFFFUL;
-        result += (u32 ? __builtin_clz(u32) : 32);
-    }
-    return result;
+int __builtin_ctz(unsigned x) {
+    unsigned long ret;
+    _BitScanForward(&ret, x);
+    return (int)ret;
 }
 
-uint32_t __builtin_clzll(uint64_t value)
-{
-    if (value == 0)
-        return 64;
-    uint32_t msh = (uint32_t)(value >> 32);
-    uint32_t lsh = (uint32_t)(value & 0xFFFFFFFF);
-    if (msh != 0)
-        return __builtin_clz(msh);
-    return 32 + __builtin_clz(lsh);
+int __builtin_ctzll(unsigned long long x) {
+    unsigned long ret;
+    _BitScanForward64(&ret, x);
+    return (int)ret;
 }
+
+int __builtin_ctzl(unsigned long x) {
+    return sizeof(x) == 8 ? __builtin_ctzll(x) : __builtin_ctz((uint32_t)x);
+}
+
+int __builtin_clz(unsigned x) {
+    //unsigned long ret;
+    //_BitScanReverse(&ret, x);
+    //return (int)(31 ^ ret);
+    return (int)__lzcnt(x);
+}
+
+int __builtin_clzll(unsigned long long x) {
+    //unsigned long ret;
+    //_BitScanReverse64(&ret, x);
+    //return (int)(63 ^ ret);
+    return (int)__lzcnt64(x);
+}
+
+int __builtin_clzl(unsigned long x) {
+    return sizeof(x) == 8 ? __builtin_clzll(x) : __builtin_clz((uint32_t)x);
+}
+
+#ifdef __cplusplus
+int __builtin_ctzl(unsigned long long x) {
+    return __builtin_ctzll(x);
+}
+
+int __builtin_clzl(unsigned long long x) {
+    return __builtin_clzll(x);
+}
+#endif
 #endif
