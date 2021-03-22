a0b91fa6b6f7c74c4fefca4f20f3e5a5f6ac4078
Aang23 <qwerty15@gmx.fr>
2021-03-22 18:53:09 +0100

    Cleanup some code

---
+++ src-core/init.cpp
@@ -0,0 +1,24 @@
+#include "init.h"
+#include "logger.h"
+#include "module.h"
+#include "pipeline.h"
+
+void initSatdump()
+{
+    logger->info("   _____       __  ____                      ");
+    logger->info("  / ___/____ _/ /_/ __ \\__  ______ ___  ____ ");
+    logger->info("  \\__ \\/ __ `/ __/ / / / / / / __ `__ \\/ __ \\");
+    logger->info(" ___/ / /_/ / /_/ /_/ / /_/ / / / / / / /_/ /");
+    logger->info("/____/\\__,_/\\__/_____/\\__,_/_/ /_/ /_/ .___/ ");
+    logger->info("                                    /_/      ");
+    logger->info("Starting SatDump v1.0");
+    logger->info("");
+
+    registerModules();
+
+    loadPipelines("pipelines.json");
+
+    logger->debug("Registered pipelines :");
+    for (Pipeline &pipeline : pipelines)
+        logger->debug(" - " + pipeline.name);
+}
