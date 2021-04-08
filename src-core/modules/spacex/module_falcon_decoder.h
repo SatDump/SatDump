e1503986d7c47fa9b9f100578c0f2bc769644964
Oleg Kutkov <elenbert@gmail.com>
2021-04-08 23:51:45 +0300

    Add video transcoder module, initially designed for SpaceX Falcon 9 stream

--- src-core/modules/spacex/module_falcon_decoder.h
+++ src-core/modules/spacex/module_falcon_decoder.h
@@ -4,26 +4,33 @@
 #include <complex>
 #include <fstream>
 #include <dsp/random.h>
+#include "falcon_video_encoder.hpp"
 
 namespace spacex
 {
     class FalconDecoderModule : public ProcessingModule
     {
-    protected:
-        std::ifstream data_in;
-        std::ofstream data_out;
-        std::atomic<size_t> filesize;
-        std::atomic<size_t> progress;
-
     public:
         FalconDecoderModule(std::string input_file, std::string output_file_hint, std::map<std::string, std::string> parameters);
         ~FalconDecoderModule();
         void process();
         void drawUI(bool window);
 
+    protected:
+        std::ifstream data_in;
+        std::ofstream data_out;
+        std::atomic<size_t> filesize;
+        std::atomic<size_t> progress;
+
+
     public:
         static std::string getID();
         static std::vector<std::string> getParameters();
         static std::shared_ptr<ProcessingModule> getInstance(std::string input_file, std::string output_file_hint, std::map<std::string, std::string> parameters);
+
+    private:
+#ifdef USE_VIDEO_ENCODER
+        std::unique_ptr<FalconVideoEncoder> videoStreamEnc;
+#endif
     };
 } // namespace falcon
