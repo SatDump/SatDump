df6cb7545c50a000ef9cfaae25a343956f292bab
Aang23 <qwerty15@gmx.fr>
2021-04-10 15:27:07 +0200

    Add FY-3 ERM Decoder

---
+++ src-core/modules/fengyun/instruments/erm/erm_reader.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "modules/common/ccsds/ccsds_1_0_1024/ccsds.h"
+#include <vector>
+
+#define cimg_use_png
+#define cimg_display 0
+#include "CImg.h"
+
+namespace fengyun
+{
+    namespace erm
+    {
+        class ERMImage
+        {
+        public:
+            unsigned short imageData[151 * 32];
+            cimg_library::CImg<unsigned short> getImage();
+            int mk = -1;
+            int lastMkMatch;
+        };
+
+        class ERMReader
+        {
+        private:
+            std::vector<ERMImage> imageVector;
+
+        public:
+            ERMReader();
+            ~ERMReader();
+            int lines;
+            void work(ccsds::ccsds_1_0_1024::CCSDSPacket &packet);
+            cimg_library::CImg<unsigned short> getChannel();
+        };
+    }
+}
