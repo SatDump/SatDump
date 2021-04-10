2e9d5e618478241370ab033cf7d4b65da57e6ad0
Aang23 <qwerty15@gmx.fr>
2021-04-10 21:16:16 +0200

    Forgot DLL exports...

--- src-core/imgui/imgui_image.h
+++ src-core/imgui/imgui_image.h
@@ -3,9 +3,10 @@
 #include "imgui.h"
 #include <cstdint>
 #include <functional>
+#include "dll_export.h"
 
-extern std::function<unsigned int()> makeImageTexture;
-extern std::function<void(unsigned int, uint32_t *, int, int)> updateImageTexture;
-extern std::function<void(unsigned int)> deleteImageTexture;
+SATDUMP_DLL extern std::function<unsigned int()> makeImageTexture;
+SATDUMP_DLL extern std::function<void(unsigned int, uint32_t *, int, int)> updateImageTexture;
+SATDUMP_DLL extern std::function<void(unsigned int)> deleteImageTexture;
 
 void ushort_to_rgba(uint16_t *input, uint32_t *output, int size, int channels = 1);
