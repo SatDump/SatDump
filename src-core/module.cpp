b8cc8072c71c3130bd5dfff44ddaa9752e918b6f
Aang23 <qwerty15@gmx.fr>
2021-06-02 23:24:22 +0200

    Jason-3 Poseidon and AMR-2 support

--- src-core/module.cpp
+++ src-core/module.cpp
@@ -140,6 +140,7 @@ SATDUMP_DLL std::map<std::string, std::function<std::shared_ptr<ProcessingModule
 
 #include "modules/jason3/module_jason3_decoder.h"
 #include "modules/jason3/instruments/poseidon/module_jason3_poseidon.h"
+#include "modules/jason3/instruments/amr2/module_jason3_amr2.h"
 
 void registerModules()
 {
@@ -252,6 +253,7 @@ void registerModules()
     // Jason-3
     REGISTER_MODULE(jason3::Jason3DecoderModule);
     REGISTER_MODULE(jason3::poseidon::Jason3PoseidonDecoderModule);
+    REGISTER_MODULE(jason3::amr2::Jason3AMR2DecoderModule);
 
     // Log them out
     logger->debug("Registered modules (" + std::to_string(modules_registry.size()) + ") : ");
