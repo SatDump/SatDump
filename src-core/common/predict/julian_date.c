9a13f2213b166f255861fc181f0bbe752b04b0b5
Aang23 <qwerty15@gmx.fr>
2021-06-03 15:11:21 +0200

    More predict Windows fixes...

--- src-core/common/predict/julian_date.c
+++ src-core/common/predict/julian_date.c
@@ -11,19 +11,27 @@
  * \param timeinfo_utc Broken down time, assumed to be in UTC
  * \return Time in UTC
  **/
-time_t mktime_utc(const struct tm* timeinfo_utc)
+time_t mktime_utc(const struct tm *timeinfo_utc)
 {
 	time_t curr_time = time(NULL);
 	int timezone_diff = 0; //deviation of the current timezone from UTC in seconds
 
 	//get UTC time, interpret resulting tm as a localtime
 	struct tm timeinfo_gmt;
+#ifdef _WIN32_
+	gmtime(&curr_time, &timeinfo_gmt);
+#else
 	gmtime_r(&curr_time, &timeinfo_gmt);
+#endif
 	time_t time_gmt = mktime(&timeinfo_gmt);
 
 	//get localtime, interpret resulting tm as localtime
 	struct tm timeinfo_local;
+#ifdef _WIN32_
+	localtime(&curr_time, &timeinfo_local);
+#else
 	localtime_r(&curr_time, &timeinfo_local);
+#endif
 	time_t time_local = mktime(&timeinfo_local);
 
 	//find the time difference between the two interpretations
@@ -54,7 +62,7 @@ time_t get_julian_start_day()
 	start_time.tm_hour = 0;
 	start_time.tm_mday = 31;
 	start_time.tm_mon = 11;
-	start_time.tm_year = 1979-1900;
+	start_time.tm_year = 1979 - 1900;
 	start_time.tm_isdst = 0;
 	return mktime_utc(&start_time);
 }
@@ -63,17 +71,21 @@ predict_julian_date_t predict_to_julian(time_t input_time)
 {
 	//get number of seconds since 1979-12-31 00:00:00 UTC, convert to days
 	double seconds = difftime(input_time, get_julian_start_day());
-	return seconds/SECONDS_PER_DAY;
+	return seconds / SECONDS_PER_DAY;
 }
 
 time_t predict_from_julian(predict_julian_date_t date)
 {
-	double seconds_since = date*SECONDS_PER_DAY;
+	double seconds_since = date * SECONDS_PER_DAY;
 	time_t ret_time = get_julian_start_day();
-	
+
 	//add number of seconds since julian start day to the julian start day, get current time_t
 	struct tm timeinfo;
-	gmtime_r(&ret_time, &timeinfo); 
+#ifdef _WIN32_
+	gmtime(&ret_time, &timeinfo);
+#else
+	gmtime_r(&ret_time, &timeinfo);
+#endif
 	timeinfo.tm_sec += seconds_since;
 	ret_time = mktime_utc(&timeinfo);
 	return ret_time;
