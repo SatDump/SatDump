ed988ce1bc2c49552a1a0b81e3a7109787d2bc36
Aang23 <qwerty15@gmx.fr>
2021-06-12 21:14:32 +0200

    Add light theme

--- src-ui/settingsui.cpp
+++ src-ui/settingsui.cpp
@@ -2,13 +2,28 @@
 #include "imgui/imgui.h"
 #include "settings.h"
 
+bool use_light_theme;
+
+void parseSettingsOrDefaults()
+{
+    if (settings.contains("use_light_theme"))
+        use_light_theme = settings["use_light_theme"].get<int>();
+    else
+        use_light_theme = false;
+}
+
 void renderSettings(int wwidth, int wheight)
 {
-    ImGui::Text("To be implemented soon...");
+    ImGui::Checkbox("Use Light Theme", &use_light_theme);
+
     ImGui::BeginGroup();
     if (ImGui::Button("Save"))
+    {
+        settings["use_light_theme"] = use_light_theme;
+
         saveSettings();
+    }
     ImGui::SameLine();
-    ImGui::TextColored(ImColor::HSV(0 / 360.0, 1, 1, 1.0), "Restarting SatDump may be necessary to apply some settings");
+    ImGui::TextColored(ImColor::HSV(0 / 360.0, 1, 1, 1.0), "Restarting SatDump may be necessary to apply some settings properly!");
     ImGui::EndGroup();
 }
