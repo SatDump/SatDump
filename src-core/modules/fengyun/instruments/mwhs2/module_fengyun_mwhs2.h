2836e5b27cf91c17d71a37d05f9de8a7c9f9e528
Aang23 <qwerty15@gmx.fr>
2021-06-22 14:56:49 +0200

    FengYun MWHS-2 support

---
+++ src-core/modules/fengyun/instruments/mwhs2/module_fengyun_mwhs2.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "module.h"
+
+namespace fengyun
+{
+    namespace mwhs2
+    {
+        class FengyunMWHS2DecoderModule : public ProcessingModule
+        {
+        protected:
+            std::atomic<size_t> filesize;
+            std::atomic<size_t> progress;
+
+        public:
+            FengyunMWHS2DecoderModule(std::string input_file, std::string output_file_hint, std::map<std::string, std::string> parameters);
+            void process();
+            void drawUI(bool window);
+
+        public:
+            static std::string getID();
+            static std::vector<std::string> getParameters();
+            static std::shared_ptr<ProcessingModule> getInstance(std::string input_file, std::string output_file_hint, std::map<std::string, std::string> parameters);
+        };
+    } // namespace erm
+} // namespace fengyun
