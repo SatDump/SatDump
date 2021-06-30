64205d4fd86a97de60e63e0c2401708695a15f21
Aang23 <qwerty15@gmx.fr>
2021-06-30 16:08:15 +0200

    Add RTL-TCP Support

---
+++ libsdr/sdr/rtl_tcp/rtl_tcp_client.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef DISABLE_SDR_RTLTCP
+#include <string>
+
+class RTLTCPClient
+{
+private:
+#ifdef _WIN32
+    SOCKET socket_fd;
+#else
+    int socket_fd;
+#endif
+    bool is_connected;
+
+public:
+    RTLTCPClient();
+
+    bool connectClient(std::string address, int port);
+    void disconnect();
+
+    void receiveSamples(uint8_t *buffer, int size);
+
+    void sendCmd(uint8_t cmd, uint32_t prm);
+
+    void setSamplerate(double samplerate);
+    void setFrequency(double frequency);
+    void setGainMode(bool mode);
+    void setGain(int gain);
+    void setAGCMode(bool enable);
+};
+#endif
