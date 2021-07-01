c82e086b32f740e1360d72639bea34b26a380c8d
Aang23 <qwerty15@gmx.fr>
2021-07-02 00:35:34 +0200

    Update CCSDS timestamp parsing

--- src-core/common/ccsds/ccsds_time.h
+++ src-core/common/ccsds/ccsds_time.h
@@ -5,13 +5,13 @@
 namespace ccsds
 {
     template <typename PKT_T>
-    time_t parseCCSDSTime(PKT_T &pkt, int offset = 0)
+    time_t parseCCSDSTime(PKT_T &pkt, int offset = 0, int ms_scale = 1000)
     {
         uint16_t days = pkt.payload[0] << 8 | pkt.payload[1];
         uint32_t milliseconds_of_day = pkt.payload[2] << 24 | pkt.payload[3] << 16 | pkt.payload[4] << 8 | pkt.payload[5];
         //uint16_t milliseconds_of_millisecond = pkt.payload[6] << 8 | pkt.payload[7];
 
-        return (offset + days) * 86400 + milliseconds_of_day;
+        return (offset + days) * 86400 + milliseconds_of_day / ms_scale;
     }
 
     template <typename PKT_T>
