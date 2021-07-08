17ca80c6f7aa751b9b2d2d24bb5d10ff3f1c948e
Aang23 <qwerty15@gmx.fr>
2021-07-08 23:28:45 +0200

    MetOp SEM Decoder

---
+++ src-core/modules/metop/instruments/sem/sem_reader.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "common/ccsds/ccsds.h"
+#include <cmath>
+#include <map>
+#define cimg_use_png
+#define cimg_display 0
+#include "CImg.h"
+#include "common/predict/predict.h"
+
+namespace metop
+{
+    namespace sem
+    {
+        class SEMReader
+        {
+        private:
+            predict_orbital_elements_t *metop_object;
+            predict_position metop_orbit;
+            cimg_library::CImg<unsigned char> map_image;
+
+        public:
+            SEMReader(int norad);
+            ~SEMReader();
+
+            void work(ccsds::CCSDSPacket &packet);
+            cimg_library::CImg<unsigned char> getImage();
+        };
+    } // namespace modis
+} // namespace eos
