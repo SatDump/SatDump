aaa52443d77e12f418455458cea21eda254a2973
Aang23 <qwerty15@gmx.fr>
2021-08-08 00:10:32 +0200

    New faster QPSK xRIT Decoder

--- src-core/modules/xrit/module_xrit_decoder.h
+++ src-core/modules/xrit/module_xrit_decoder.h
@@ -6,6 +6,9 @@
 #include <fstream>
 #include "common/dsp/lib/random.h"
 
+#include "stream_viterbi.h"
+#include "common/ccsds/ccsds_1_0_1024/deframer.h"
+
 namespace xrit
 {
     class XRITDecoderModule : public ProcessingModule
@@ -25,6 +28,13 @@ namespace xrit
         int cor;
 
         viterbi::Viterbi27 viterbi;
+
+        // QPSK Variant
+        int d_viterbi_outsync_after;
+        float d_viterbi_ber_threasold;
+        xRITViterbi stream_viterbi;
+        uint8_t *viterbi_out;
+        ccsds::ccsds_1_0_1024::CADUDeframer deframer;
 
         // UI Stuff
         float ber_history[200];
