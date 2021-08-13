d7f8284e36502c142a035bb80c302b716dbc7925
Aang23 <qwerty15@gmx.fr>
2021-08-13 12:00:18 +0200

    Allow selecting a device in the ingestor

--- libsdr/sdr/sdr.h
+++ libsdr/sdr/sdr.h
@@ -11,6 +11,7 @@
 
 enum sdr_device_type
 {
+    NONE,
     AIRSPY,
     RTLSDR,
     HACKRF,
@@ -48,4 +49,5 @@ void initSDRs();
 std::vector<std::tuple<std::string, sdr_device_type, uint64_t>> getAllDevices();
 std::map<std::string, std::string> drawParamsUIForID(std::vector<std::tuple<std::string, sdr_device_type, uint64_t>> devList, int num);
 std::string getDeviceIDStringByID(std::vector<std::tuple<std::string, sdr_device_type, uint64_t>> devList, int num);
+sdr_device_type getDeviceIDbyIDString(std::string idString);
 std::shared_ptr<SDRDevice> getDeviceByID(std::vector<std::tuple<std::string, sdr_device_type, uint64_t>> devList, std::map<std::string, std::string> parameters, int num);
