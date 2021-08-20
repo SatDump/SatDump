fb3965e6a41e9380941878c8ecb9055729a52288
Aang23 <qwerty15@gmx.fr>
2021-08-20 20:34:22 +0200

    Add standard GEOS projection code

---
+++ src-core/common/projection/geos.h
@@ -0,0 +1,39 @@
+#pragma once
+
+/*
+Implementation of a standard GEOS projection, adapted from libproj.
+Some variables are hardcoded for the intended usecase, making some
+degree of tuning unecessary.
+*/
+namespace projection
+{
+    class GEOSProjection
+    {
+    private:
+        double h;
+        double radius_p;
+        double radius_p2;
+        double radius_p_inv2;
+        double radius_g;
+        double radius_g_1;
+        double C;
+        int flip_axis;
+
+        double phi0;
+        double a;
+        double es;
+        double one_es;
+
+        double lon_0;
+
+    public:
+        GEOSProjection(double height, double longitude, bool sweep_x = false)
+        {
+            init(height, longitude, sweep_x);
+        }
+
+        void init(double height, double longitude, bool sweep_x = false);
+        void forward(double lon, double lat, double &x, double &y);
+        void inverse(double x, double y, double &lon, double &lat);
+    };
+};
