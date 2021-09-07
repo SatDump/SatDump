71c349aae42067b551bc95e1fda387160a56dcad
Aang23 <qwerty15@gmx.fr>
2021-09-07 13:27:58 +0200

    MWTS-2 Decoder

--- src-core/common/ccsds/ccsds_time.h
+++ src-core/common/ccsds/ccsds_time.h
@@ -5,6 +5,7 @@
 
 namespace ccsds
 {
-    time_t parseCCSDSTime(CCSDSPacket &pkt, int offset = 0, int ms_scale = 1000);                            // Parse timestamp with 1 second accuracy
-    double parseCCSDSTimeFull(CCSDSPacket &pkt, int offset, int ms_scale = 1000, int ns_of_ms_scale = 1000); // Parse timestamp with full accuracy
+    time_t parseCCSDSTime(CCSDSPacket &pkt, int offset = 0, int ms_scale = 1000);                               // Parse timestamp with 1 second accuracy
+    double parseCCSDSTimeFull(CCSDSPacket &pkt, int offset, int ms_scale = 1000, int ns_of_ms_scale = 1000);    // Parse timestamp with full accuracy
+    double parseCCSDSTimeFullRaw(uint8_t *data, int offset, int ms_scale = 1000, int ns_of_ms_scale = 1000); // Parse timestamp with full accuracy, from a raw buffer
 }
---
+++ src-core/modules/fengyun/instruments/mwts2/mwts2_reader.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "common/ccsds/ccsds.h"
+#define cimg_use_png
+#define cimg_display 0
+#include "CImg.h"
+#include <map>
+
+namespace fengyun
+{
+    namespace mwts2
+    {
+        class MWTS2Reader
+        {
+        private:
+            std::map<double, std::array<std::array<unsigned short, 90>, 18>> imageData;
+            double lastTime;
+            unsigned short lineBuf[1000];
+
+        public:
+            MWTS2Reader();
+            ~MWTS2Reader();
+            int lines;
+            std::vector<double> timestamps;
+            void work(ccsds::CCSDSPacket &packet);
+            cimg_library::CImg<unsigned short> getChannel(int channel);
+        };
+    }
+}
