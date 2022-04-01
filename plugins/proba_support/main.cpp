c7dee6e695280bc20210dc4beeb7b74ec2cc7bdb
Aang23 <qwerty15@gmx.fr>
2022-04-01 11:49:51 +0200

    Delete useless Vegetation stuff

--- plugins/proba_support/main.cpp
+++ plugins/proba_support/main.cpp
@@ -5,7 +5,6 @@
 #include "proba/instruments/swap/module_proba_swap.h"
 #include "proba/instruments/chris/module_proba_chris.h"
 #include "proba/instruments/hrc/module_proba_hrc.h"
-#include "proba/instruments/vegetation/module_proba_vegetation.h"
 
 class ProbaSupport : public satdump::Plugin
 {
@@ -25,7 +24,6 @@ public:
         REGISTER_MODULE_EXTERNAL(evt.modules_registry, proba::swap::ProbaSWAPDecoderModule);
         REGISTER_MODULE_EXTERNAL(evt.modules_registry, proba::chris::ProbaCHRISDecoderModule);
         REGISTER_MODULE_EXTERNAL(evt.modules_registry, proba::hrc::ProbaHRCDecoderModule);
-        REGISTER_MODULE_EXTERNAL(evt.modules_registry, proba::vegetation::ProbaVegetationDecoderModule);
     }
 };
 
