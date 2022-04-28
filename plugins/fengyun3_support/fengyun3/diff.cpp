6c42da1195f577a909b74149e5f64c00431fe718
Aang23 <qwerty15@gmx.fr>
2022-04-28 19:06:56 +0200

    Redo MPT Decoder

--- plugins/fengyun3_support/fengyun3/diff.cpp
+++ plugins/fengyun3_support/fengyun3/diff.cpp
@@ -1,4 +1,5 @@
 #include "diff.h"
+#include <cmath>
 
 namespace fengyun3
 {
@@ -39,6 +40,38 @@ namespace fengyun3
                 Yout = (Yin_1 ^ Yin);
                 out[oo++] = (Xout + Yout);
             }
+        }
+    }
+
+    void FengyunDiff::work2(uint8_t *in1, uint8_t *in2, int len, uint8_t *out)
+    {
+        int oo = 0;
+
+        // Process all given samples
+        for (int ii = 0; ii < len; ii++)
+        {
+            // Push new sample
+            Xin_1 = Xin;
+            Yin_1 = Yin;
+            Xin = in1[ii] << 1;
+            Yin = in2[ii];
+
+            // Perform differential decoding
+            if (((Xin >> 1) ^ Yin) == 1)
+            {
+                Xout = (Yin_1 ^ Yin);
+                Yout = (Xin_1 ^ Xin);
+                v = (Xout << 1) + (Yout >> 1);
+            }
+            else
+            {
+                Xout = (Xin_1 ^ Xin);
+                Yout = (Yin_1 ^ Yin);
+                v = (Xout + Yout);
+            }
+
+            out[oo++] = v >> 1;
+            out[oo++] = v & 1;
         }
     }
 } // namespace fengyun
--- plugins/fengyun3_support/fengyun3/diff.h
+++ plugins/fengyun3_support/fengyun3/diff.h
@@ -6,11 +6,16 @@ namespace fengyun3
     class FengyunDiff
     {
     private:
-        unsigned char Xin_1, Yin_1, Xin, Yin, Xout, Yout;
+        unsigned char Xin_1 = 0, Yin_1 = 0, Xin = 0, Yin = 0, Xout = 0, Yout = 0;
+        uint8_t v = 0;
         char inBuf = 0;    // Counter used at the beggining
         uint8_t buffer[2]; // Smaller buffer for internal use
 
+        uint8_t mask[4] = {0b00000000, 0b11000000, 0b11110000, 0b11111100};
+
     public:
         void work(uint8_t *in, int len, uint8_t *out);
+
+        void work2(uint8_t *in1, uint8_t *in2, int len, uint8_t *out);
     };
 } // namespace fengyun
