2c9001961f708f97a5b5f5a3f65db974fa999ffa
Aang23 <qwerty15@gmx.fr>
2022-04-29 12:47:23 +0200

    Oops

--- plugins/fengyun3_support/main.cpp
+++ plugins/fengyun3_support/main.cpp
@@ -4,12 +4,6 @@
 
 #include "fengyun3/module_fengyun_ahrpt_decoder.h"
 #include "fengyun3/module_fengyun_mpt_decoder.h"
-
-#include "fengyun3/instruments/mersi1/module_fengyun_mersi1.h"
-#include "fengyun3/instruments/mersi2/module_fengyun_mersi2.h"
-
-#include "fengyun3/instruments/mersill/module_fengyun_mersill.h"
-
 #include "fengyun3/module_fy3_instruments.h"
 
 class FengYun3Support : public satdump::Plugin
@@ -29,11 +23,6 @@ public:
     {
         REGISTER_MODULE_EXTERNAL(evt.modules_registry, fengyun3::FengyunAHRPTDecoderModule);
         REGISTER_MODULE_EXTERNAL(evt.modules_registry, fengyun3::FengyunMPTDecoderModule);
-
-        REGISTER_MODULE_EXTERNAL(evt.modules_registry, fengyun3::mersi1::FengyunMERSI1DecoderModule);
-        REGISTER_MODULE_EXTERNAL(evt.modules_registry, fengyun3::mersi2::FengyunMERSI2DecoderModule);
-        REGISTER_MODULE_EXTERNAL(evt.modules_registry, fengyun3::mersill::FengyunMERSILLDecoderModule);
-
         REGISTER_MODULE_EXTERNAL(evt.modules_registry, fengyun3::instruments::FY3InstrumentsDecoderModule);
     }
 };
