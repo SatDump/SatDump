21b893c8cb66d12f92281e26cf1e9978040b80ed
Aang23 <qwerty15@gmx.fr>
2022-05-05 18:30:33 +0200

    Add SDRPlay support

---
+++ plugins/sdr_sources/sdrplay_sdr_support/main.cpp
@@ -0,0 +1,32 @@
+#include "core/plugin.h"
+#include "logger.h"
+#include "sdrplay_sdr.h"
+
+class SDRPlaySDRSupport : public satdump::Plugin
+{
+public:
+    std::string getID()
+    {
+        return "sdrplay_sdr_support";
+    }
+
+    void init()
+    {
+        satdump::eventBus->register_handler<dsp::RegisterDSPSampleSourcesEvent>(registerSources);
+
+        sdrplay_api_ErrT error = sdrplay_api_Open();
+        if (error != sdrplay_api_Success)
+        {
+            logger->error("Couuld not open the SDRPlay API, perhaps the service is not running?");
+            return;
+        }
+        logger->info("SDRPlay APi is ready!");
+    }
+
+    static void registerSources(const dsp::RegisterDSPSampleSourcesEvent &evt)
+    {
+        evt.dsp_sources_registry.insert({SDRPlaySource::getID(), {SDRPlaySource::getInstance, SDRPlaySource::getAvailableSources}});
+    }
+};
+
+PLUGIN_LOADER(SDRPlaySDRSupport)
---
+++ plugins/sdr_sources/sdrplay_sdr_support/sdrplay_sdr.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "common/dsp_sample_source/dsp_sample_source.h"
+#include <sdrplay_api.h>
+#include "logger.h"
+#include "imgui/imgui.h"
+#include "core/style.h"
+
+class SDRPlaySource : public dsp::DSPSampleSource
+{
+protected:
+    bool is_open = false, is_started = false;
+    sdrplay_api_DeviceT sdrplay_dev;
+    sdrplay_api_DeviceParamsT *dev_params = nullptr;
+    sdrplay_api_RxChannelParamsT *channel_params = nullptr;
+    sdrplay_api_CallbackFnsT callback_funcs;
+    static sdrplay_api_DeviceT devices_addresses[128];
+    static void event_callback(sdrplay_api_EventT id, sdrplay_api_TunerSelectT tuner, sdrplay_api_EventParamsT *params, void *ctx);
+    static void stream_callback(short *real, short *imag, sdrplay_api_StreamCbParamsT *params, unsigned int cnt, unsigned int reset, void *ctx);
+
+    int max_gain;
+
+    int selected_samplerate = 0;
+    std::string samplerate_option_str;
+    std::vector<uint64_t> available_samplerates;
+    uint64_t current_samplerate = 0;
+
+    int lna_gain = 0;
+    int if_gain = 20;
+    bool bias = false;
+    bool fm_notch = false;
+    bool dab_notch = false;
+    bool am_notch = false;
+    int am_port = 1;
+    int antenna_input = 0;
+    int agc_mode = 0;
+
+    void set_gains();
+    void set_bias();
+    void set_agcs();
+    void set_others();
+
+public:
+    SDRPlaySource(dsp::SourceDescriptor source) : DSPSampleSource(source) {}
+
+    ~SDRPlaySource()
+    {
+        stop();
+        close();
+    }
+
+    void set_settings(nlohmann::json settings);
+    nlohmann::json get_settings(nlohmann::json);
+
+    void open();
+    void start();
+    void stop();
+    void close();
+
+    void set_frequency(uint64_t frequency);
+
+    void drawControlUI();
+
+    void set_samplerate(uint64_t samplerate);
+    uint64_t get_samplerate();
+
+    static std::string getID() { return "sdrplay"; }
+    static std::shared_ptr<dsp::DSPSampleSource> getInstance(dsp::SourceDescriptor source) { return std::make_shared<SDRPlaySource>(source); }
+    static std::vector<dsp::SourceDescriptor> getAvailableSources();
+};
