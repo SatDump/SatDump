7e7aa45137278658f2ad91c5bf7654f8b12a8baa
Aang23 <qwerty15@gmx.fr>
2022-05-07 16:24:54 +0200

    Add SpyServer support

---
+++ plugins/sdr_sources/spyserver_support/spyserver/spyserver_client.cpp
@@ -0,0 +1,205 @@
+#include "spyserver_client.h"
+#include <volk/volk.h>
+#include <cstring>
+
+using namespace std::chrono_literals;
+
+namespace spyserver
+{
+    SpyServerClientClass::SpyServerClientClass(net::Conn conn, dsp::stream<complex_t> *out)
+    {
+        readBuf = new uint8_t[SPYSERVER_MAX_MESSAGE_BODY_SIZE];
+        writeBuf = new uint8_t[SPYSERVER_MAX_MESSAGE_BODY_SIZE];
+        client = std::move(conn);
+        output = out;
+
+        output->clearWriteStop();
+
+        sendHandshake("SatDump");
+
+        client->readAsync(sizeof(SpyServerMessageHeader), (uint8_t *)&receivedHeader, dataHandler, this);
+    }
+
+    SpyServerClientClass::~SpyServerClientClass()
+    {
+        close();
+        delete[] readBuf;
+        delete[] writeBuf;
+    }
+
+    void SpyServerClientClass::startStream()
+    {
+        output->clearWriteStop();
+        setSetting(SPYSERVER_SETTING_STREAMING_ENABLED, true);
+    }
+
+    void SpyServerClientClass::stopStream()
+    {
+        output->stopWriter();
+        setSetting(SPYSERVER_SETTING_STREAMING_ENABLED, false);
+    }
+
+    void SpyServerClientClass::close()
+    {
+        output->stopWriter();
+        client->close();
+    }
+
+    bool SpyServerClientClass::isOpen()
+    {
+        return client->isOpen();
+    }
+
+    int SpyServerClientClass::computeDigitalGain(int /*serverBits*/, int deviceGain, int decimationId)
+    {
+        if (devInfo.DeviceType == SPYSERVER_DEVICE_AIRSPY_ONE)
+        {
+            return (devInfo.MaximumGainIndex - deviceGain) + (decimationId * 3.01f);
+        }
+        else if (devInfo.DeviceType == SPYSERVER_DEVICE_AIRSPY_HF)
+        {
+            return decimationId * 3.01f;
+        }
+        else if (devInfo.DeviceType == SPYSERVER_DEVICE_RTLSDR)
+        {
+            return decimationId * 3.01f;
+        }
+        else
+        {
+            // Error, unknown device
+            return -1;
+        }
+    }
+
+    bool SpyServerClientClass::waitForDevInfo(int timeoutMS)
+    {
+        std::unique_lock lck(deviceInfoMtx);
+        auto now = std::chrono::system_clock::now();
+        deviceInfoCnd.wait_until(lck, now + (timeoutMS * 1ms), [this]()
+                                 { return deviceInfoAvailable; });
+        return deviceInfoAvailable;
+    }
+
+    void SpyServerClientClass::sendCommand(uint32_t command, void *data, int len)
+    {
+        SpyServerCommandHeader *hdr = (SpyServerCommandHeader *)writeBuf;
+        hdr->CommandType = command;
+        hdr->BodySize = len;
+        memcpy(&writeBuf[sizeof(SpyServerCommandHeader)], data, len);
+        client->write(sizeof(SpyServerCommandHeader) + len, writeBuf);
+    }
+
+    void SpyServerClientClass::sendHandshake(std::string appName)
+    {
+        int totSize = sizeof(SpyServerClientHandshake) + appName.size();
+        uint8_t *buf = new uint8_t[totSize];
+
+        SpyServerClientHandshake *cmdHandshake = (SpyServerClientHandshake *)buf;
+        cmdHandshake->ProtocolVersion = SPYSERVER_PROTOCOL_VERSION;
+
+        memcpy(&buf[sizeof(SpyServerClientHandshake)], appName.c_str(), appName.size());
+        sendCommand(SPYSERVER_CMD_HELLO, buf, totSize);
+
+        delete[] buf;
+    }
+
+    void SpyServerClientClass::setSetting(uint32_t setting, uint32_t arg)
+    {
+        SpyServerSettingTarget target;
+        target.Setting = setting;
+        target.Value = arg;
+        sendCommand(SPYSERVER_CMD_SET_SETTING, &target, sizeof(SpyServerSettingTarget));
+    }
+
+    int SpyServerClientClass::readSize(int count, uint8_t *buffer)
+    {
+        int read = 0;
+        int len = 0;
+        while (read < count)
+        {
+            len = client->read(count - read, &buffer[read]);
+            if (len <= 0)
+            {
+                return len;
+            }
+            read += len;
+        }
+        return read;
+    }
+
+    void SpyServerClientClass::dataHandler(int count, uint8_t *buf, void *ctx)
+    {
+        SpyServerClientClass *_this = (SpyServerClientClass *)ctx;
+
+        if (count < (int)sizeof(SpyServerMessageHeader))
+        {
+            _this->readSize(sizeof(SpyServerMessageHeader) - count, &buf[count]);
+        }
+
+        int size = _this->readSize(_this->receivedHeader.BodySize, _this->readBuf);
+        if (size <= 0)
+        {
+            printf("ERROR: Disconnected\n");
+            return;
+        }
+
+        //printf("MSG Proto: 0x%08X, MsgType: 0x%08X, StreamType: 0x%08X, Seq: 0x%08X, Size: %d\n", _this->receivedHeader.ProtocolID, _this->receivedHeader.MessageType, _this->receivedHeader.StreamType, _this->receivedHeader.SequenceNumber, _this->receivedHeader.BodySize);
+
+        int mtype = _this->receivedHeader.MessageType & 0xFFFF;
+        int mflags = (_this->receivedHeader.MessageType & 0xFFFF0000) >> 16;
+
+        if (mtype == SPYSERVER_MSG_TYPE_DEVICE_INFO)
+        {
+            {
+                std::lock_guard lck(_this->deviceInfoMtx);
+                SpyServerDeviceInfo *_devInfo = (SpyServerDeviceInfo *)_this->readBuf;
+                _this->devInfo = *_devInfo;
+                _this->deviceInfoAvailable = true;
+            }
+            _this->deviceInfoCnd.notify_all();
+        }
+        else if (mtype == SPYSERVER_MSG_TYPE_UINT8_IQ)
+        {
+            int sampCount = _this->receivedHeader.BodySize / (sizeof(uint8_t) * 2);
+            float gain = pow(10, (double)mflags / 20.0);
+            float scale = 1.0f / (gain * 128.0f);
+            for (int i = 0; i < sampCount; i++)
+            {
+                _this->output->writeBuf[i] = complex_t(((float)_this->readBuf[(2 * i)] - 128.0f) * scale,
+                                                                 ((float)_this->readBuf[(2 * i) + 1] - 128.0f) * scale);
+            }
+            _this->output->swap(sampCount);
+        }
+        else if (mtype == SPYSERVER_MSG_TYPE_INT16_IQ)
+        {
+            int sampCount = _this->receivedHeader.BodySize / (sizeof(int16_t) * 2);
+            float gain = pow(10, (double)mflags / 20.0);
+            volk_16i_s32f_convert_32f((float *)_this->output->writeBuf, (int16_t *)_this->readBuf, 32768.0 * gain, sampCount * 2);
+            _this->output->swap(sampCount);
+        }
+        else if (mtype == SPYSERVER_MSG_TYPE_INT24_IQ)
+        {
+            printf("ERROR: IQ format not supported\n");
+            return;
+        }
+        else if (mtype == SPYSERVER_MSG_TYPE_FLOAT_IQ)
+        {
+            int sampCount = _this->receivedHeader.BodySize / sizeof(complex_t);
+            float gain = pow(10, (double)mflags / 20.0);
+            volk_32f_s32f_multiply_32f((float *)_this->output->writeBuf, (float *)_this->readBuf, gain, sampCount * 2);
+            _this->output->swap(sampCount);
+        }
+
+        _this->client->readAsync(sizeof(SpyServerMessageHeader), (uint8_t *)&_this->receivedHeader, dataHandler, _this);
+    }
+
+    SpyServerClient connect(std::string host, uint16_t port, dsp::stream<complex_t> *out)
+    {
+        net::Conn conn = net::connect(host, port);
+        if (!conn)
+        {
+            return NULL;
+        }
+        return SpyServerClient(new SpyServerClientClass(std::move(conn), out));
+    }
+}
---
+++ plugins/sdr_sources/spyserver_support/spyserver/spyserver_protocol.h
@@ -0,0 +1,148 @@
+/*
+
+SPY Server protocol structures and constants
+Copyright (C) 2017 Youssef Touil youssef@live.com
+
+Corrections by Ryzerth.
+
+*/
+
+
+#pragma once
+
+#include <stdint.h>
+#include <limits.h>
+
+#define SPYSERVER_PROTOCOL_VERSION (((2) << 24) | ((0) << 16) | (1700))
+
+#define SPYSERVER_MAX_COMMAND_BODY_SIZE (256)
+#define SPYSERVER_MAX_MESSAGE_BODY_SIZE (1 << 20)
+#define SPYSERVER_MAX_DISPLAY_PIXELS (1 << 15)
+#define SPYSERVER_MIN_DISPLAY_PIXELS (100)
+#define SPYSERVER_MAX_FFT_DB_RANGE (150)
+#define SPYSERVER_MIN_FFT_DB_RANGE (10)
+#define SPYSERVER_MAX_FFT_DB_OFFSET (100)
+
+enum SpyServerDeviceType {
+    SPYSERVER_DEVICE_INVALID        = 0,
+    SPYSERVER_DEVICE_AIRSPY_ONE     = 1,
+    SPYSERVER_DEVICE_AIRSPY_HF      = 2,
+    SPYSERVER_DEVICE_RTLSDR         = 3,
+};
+
+enum SpyServerCommandType {
+    SPYSERVER_CMD_HELLO             = 0,
+    SPYSERVER_CMD_SET_SETTING       = 2,
+    SPYSERVER_CMD_PING              = 3,
+};
+
+enum SpyServerSettingType {
+    SPYSERVER_SETTING_STREAMING_MODE        = 0,
+    SPYSERVER_SETTING_STREAMING_ENABLED     = 1,
+    SPYSERVER_SETTING_GAIN                  = 2,
+
+    SPYSERVER_SETTING_IQ_FORMAT             = 100,    // 0x64
+    SPYSERVER_SETTING_IQ_FREQUENCY          = 101,    // 0x65
+    SPYSERVER_SETTING_IQ_DECIMATION         = 102,    // 0x66
+    SPYSERVER_SETTING_IQ_DIGITAL_GAIN       = 103,    // 0x67
+
+    SPYSERVER_SETTING_FFT_FORMAT            = 200,    // 0xc8
+    SPYSERVER_SETTING_FFT_FREQUENCY         = 201,    // 0xc9
+    SPYSERVER_SETTING_FFT_DECIMATION        = 202,    // 0xca
+    SPYSERVER_SETTING_FFT_DB_OFFSET         = 203,    // 0xcb
+    SPYSERVER_SETTING_FFT_DB_RANGE          = 204,    // 0xcc
+    SPYSERVER_SETTING_FFT_DISPLAY_PIXELS    = 205,    // 0xcd
+};
+
+enum SpyServerStreamType {
+    SPYSERVER_STREAM_TYPE_STATUS    = 0,
+    SPYSERVER_STREAM_TYPE_IQ        = 1,
+    SPYSERVER_STREAM_TYPE_AF        = 2,
+    SPYSERVER_STREAM_TYPE_FFT       = 4,
+};
+
+enum SpyServerStreamingMode {
+    SPYSERVER_STREAM_MODE_IQ_ONLY       = SPYSERVER_STREAM_TYPE_IQ,                     // 0x01
+    SPYSERVER_STREAM_MODE_AF_ONLY       = SPYSERVER_STREAM_TYPE_AF,                     // 0x02
+    SPYSERVER_STREAM_MODE_FFT_ONLY      = SPYSERVER_STREAM_TYPE_FFT,                    // 0x04
+    SPYSERVER_STREAM_MODE_FFT_IQ        = SPYSERVER_STREAM_TYPE_FFT | SPYSERVER_STREAM_TYPE_IQ,   // 0x05
+    SPYSERVER_STREAM_MODE_FFT_AF        = SPYSERVER_STREAM_TYPE_FFT | SPYSERVER_STREAM_TYPE_AF,   // 0x06
+};
+
+enum SpyServerStreamFormat {
+    SPYSERVER_STREAM_FORMAT_INVALID     = 0,
+    SPYSERVER_STREAM_FORMAT_UINT8       = 1,
+    SPYSERVER_STREAM_FORMAT_INT16       = 2,
+    SPYSERVER_STREAM_FORMAT_INT24       = 3,
+    SPYSERVER_STREAM_FORMAT_FLOAT       = 4,
+    SPYSERVER_STREAM_FORMAT_DINT4       = 5,
+};
+
+enum SpyServerMessageType {
+    SPYSERVER_MSG_TYPE_DEVICE_INFO      = 0,
+    SPYSERVER_MSG_TYPE_CLIENT_SYNC      = 1,
+    SPYSERVER_MSG_TYPE_PONG             = 2,
+    SPYSERVER_MSG_TYPE_READ_SETTING     = 3,
+
+    SPYSERVER_MSG_TYPE_UINT8_IQ         = 100,     // 0x64
+    SPYSERVER_MSG_TYPE_INT16_IQ         = 101,     // 0x65
+    SPYSERVER_MSG_TYPE_INT24_IQ         = 102,     // 0x66
+    SPYSERVER_MSG_TYPE_FLOAT_IQ         = 103,     // 0x67
+
+    SPYSERVER_MSG_TYPE_UINT8_AF         = 200,    // 0xc8
+    SPYSERVER_MSG_TYPE_INT16_AF         = 201,    // 0xc9
+    SPYSERVER_MSG_TYPE_INT24_AF         = 202,    // 0xca
+    SPYSERVER_MSG_TYPE_FLOAT_AF         = 203,    // 0xcb
+
+    SPYSERVER_MSG_TYPE_DINT4_FFT        = 300,    //0x12C
+    SPYSERVER_MSG_TYPE_UINT8_FFT        = 301,    //0x12D
+};
+
+struct SpyServerClientHandshake {
+    uint32_t ProtocolVersion;
+};
+
+struct SpyServerCommandHeader {
+    uint32_t CommandType;
+    uint32_t BodySize;
+};
+
+struct SpyServerSettingTarget {
+    uint32_t Setting;
+    uint32_t Value;
+};
+
+struct SpyServerMessageHeader {
+    uint32_t ProtocolID;
+    uint32_t MessageType;
+    uint32_t StreamType;
+    uint32_t SequenceNumber;
+    uint32_t BodySize;
+};
+
+struct SpyServerDeviceInfo {
+    uint32_t DeviceType;
+    uint32_t DeviceSerial;
+    uint32_t MaximumSampleRate;
+    uint32_t MaximumBandwidth;
+    uint32_t DecimationStageCount;
+    uint32_t GainStageCount;
+    uint32_t MaximumGainIndex;
+    uint32_t MinimumFrequency;
+    uint32_t MaximumFrequency;
+    uint32_t Resolution;
+    uint32_t MinimumIQDecimation;
+    uint32_t ForcedIQFormat;
+};
+
+struct SpyServerClientSync {
+    uint32_t CanControl;
+    uint32_t Gain;
+    uint32_t DeviceCenterFrequency;
+    uint32_t IQCenterFrequency;
+    uint32_t FFTCenterFrequency;
+    uint32_t MinimumIQCenterFrequency;
+    uint32_t MaximumIQCenterFrequency;
+    uint32_t MinimumFFTCenterFrequency;
+    uint32_t MaximumFFTCenterFrequency;
+};
