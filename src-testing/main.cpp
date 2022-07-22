eacb14dfacd78f00f55b010c777d86a3c14207db
Aang23 <qwerty15@gmx.fr>
2022-07-22 22:52:07 +0200

    Some SUVI test for Lazz

--- src-testing/main.cpp
+++ src-testing/main.cpp
@@ -11,15 +11,95 @@
  **********************************************************************/
 
 #include "logger.h"
-#include "products/image_products.h"
+#include "common/image/image.h"
+#include "libs/rapidxml.hpp"
+#include <filesystem>
+#include <fstream>
 
-#include "common/projection/sat_proj/sat_proj.h"
-#include "common/geodetic/vincentys_calculations.h"
+// XML Parser
+rapidxml::xml_document<> doc;
+rapidxml::xml_node<> *root_node = NULL;
 
 int main(int argc, char *argv[])
 {
     initLogger();
 
+    std::filesystem::recursive_directory_iterator pluginIterator(argv[1]);
+    std::error_code iteratorError;
+    while (pluginIterator != std::filesystem::recursive_directory_iterator())
+    {
+        if (pluginIterator->path().filename().string().find(".png") != std::string::npos)
+        {
+            std::string png_path = pluginIterator->path().string();
+            std::string xml_path = png_path.substr(0, png_path.size() - 4) + ".xml";
+
+            std::string png_path2 = png_path.substr(0, png_path.size() - 4) + "_CALIB.png";
+
+            if (!std::filesystem::exists(xml_path))
+            {
+                pluginIterator.increment(iteratorError);
+                continue;
+            }
+
+            image::Image<uint16_t> suvi_image;
+            suvi_image.load_png(png_path);
+
+            std::ifstream xml_ifs(xml_path);
+            std::string xml_content((std::istreambuf_iterator<char>(xml_ifs)),
+                                    (std::istreambuf_iterator<char>()));
+
+            doc.parse<0>((char *)xml_content.c_str());
+            root_node = doc.first_node("netcdf");
+
+            float img_min, img_max;
+            std::string sci_obj;
+
+            for (rapidxml::xml_node<> *sat_node = root_node->first_node("variable"); sat_node; sat_node = sat_node->next_sibling())
+            {
+                //  for (rapidxml::xml_node<> *tle_node = sat_tle_node->first_node("attribute"); tle_node; tle_node = tle_node->next_sibling())
+                if (sat_node->first_attribute("name") != NULL)
+                {
+                    if (std::string(sat_node->first_attribute("name")->value()) == "IMG_MIN")
+                        img_min = std::stof(sat_node->first_node("values")->value());
+                    if (std::string(sat_node->first_attribute("name")->value()) == "IMG_MAX")
+                        img_max = std::stof(sat_node->first_node("values")->value());
+                    if (std::string(sat_node->first_attribute("name")->value()) == "SCI_OBJ")
+                        sci_obj = std::string(sat_node->first_node("values")->value());
+                }
+            }
+
+            if (sci_obj != "Fe_XII_195.1A_long_exposure")
+            {
+                pluginIterator.increment(iteratorError);
+                continue;
+            }
+
+            logger->info("Image Max {:f} Min {:f}, product {:s}", img_max, img_min, sci_obj.c_str());
+
+            for (size_t px = 0; px < suvi_image.width() * suvi_image.height(); px++)
+            {
+                double radiance = img_min + (suvi_image[px] / 65535.0) * (img_max - img_min);
+
+                const double mmm_min = -1;
+                const double mmm_max = 24;
+
+                double calib_v = ((radiance - mmm_min) / (mmm_max - mmm_min)) * 65535.0;
+
+                if (calib_v < 0)
+                    calib_v = 0;
+                if (calib_v > 65535)
+                    calib_v = 65535;
+
+                suvi_image[px] = calib_v;
+            }
+
+            suvi_image.save_png(png_path2);
+        }
+
+        pluginIterator.increment(iteratorError);
+    }
+
+#if 0
     image::Image<uint8_t> image_1, image_2, image_3, image_rgb;
 
     image_1.load_png(argv[1]);
@@ -35,4 +115,5 @@ int main(int argc, char *argv[])
 
     logger->info("Saving");
     image_rgb.save_png("msu_compo.png");
+#endif
 }
