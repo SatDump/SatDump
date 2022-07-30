456b8a6c97af1564b0ee5671d1dd82f4360ca6da
Aang23 <qwerty15@gmx.fr>
2022-07-30 20:19:55 +0200

    Don't segfault on exit

--- src-interface/recorder/recorder.cpp
+++ src-interface/recorder/recorder.cpp
@@ -69,6 +69,7 @@ namespace satdump
     {
         source_ptr->stop();
         source_ptr->close();
+        splitter->input_stream = std::make_shared<dsp::stream<complex_t>>();
         splitter->stop();
         fft->stop();
         file_sink->stop();
