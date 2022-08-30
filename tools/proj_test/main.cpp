fa83fc84aaeb9c2630e2240cd918567393d2d402
Aang23 <qwerty15@gmx.fr>
2022-08-30 20:13:49 +0200

    Test stuff

--- tools/proj_test/main.cpp
+++ tools/proj_test/main.cpp
@@ -166,7 +166,7 @@ int main(int argc, char *argv[])
 
     logger->trace("\n" + img_pro.contents.dump(4));
 
-    std::vector<satdump::projection::GCP> gcps = satdump::gcp_compute::compute_gcps(loadJsonFile(resources::getResourcePath("projections_settings/fengyun_d_mersi2.json")), img_pro.get_tle(), img_pro.get_timestamps(0));
+    std::vector<satdump::projection::GCP> gcps = satdump::gcp_compute::compute_gcps(loadJsonFile(resources::getResourcePath("projections_settings/metop_abc_avhrr.json")), img_pro.get_tle(), img_pro.get_timestamps(0));
 
     satdump::ImageCompositeCfg rgb_cfg;
     rgb_cfg.equation = "ch3,ch2,ch1"; //"(ch3 * 0.4 + ch2 * 0.6) * 2.2 - 0.15, ch2 * 2.2 - 0.15, ch1 * 2.2 - 0.15";
@@ -179,8 +179,8 @@ int main(int argc, char *argv[])
     satdump::warp::WarpOperation operation;
     operation.ground_control_points = gcps;
     operation.input_image = satdump::make_composite_from_product(img_pro, rgb_cfg);
-    operation.output_width = 2048 * 20;
-    operation.output_height = 1024 * 20;
+    operation.output_width = 2048 * 10;
+    operation.output_height = 1024 * 10;
 
     satdump::warp::ImageWarper warper;
     warper.op = operation;
@@ -245,7 +245,7 @@ int main(int argc, char *argv[])
     logger->info("Saving...");
 #endif
 
-   // result.output_image.save_png("test.png");
+    // result.output_image.save_png("test.png");
 
 #if 1
     geodetic::projection::EquirectangularProjection projector;
