0e66eacbf7857b3ea17dccd57cdf0b26bb1b1bcf
Aang23 <qwerty15@gmx.fr>
2022-12-03 00:05:47 +0100

    Fix GOES Rice decompression errors I think?

--- src-core/libs/aec/config.h
+++ src-core/libs/aec/config.h
@@ -1,5 +1,8 @@
 #define HAVE_MALLOC_H 1
 #define HAVE_STDINT_H 1
 /* #undef WORDS_BIGENDIAN */
+#if defined(_WIN32)
+#define HAVE_BSR64 1
+#else
 #define HAVE_DECL___BUILTIN_CLZLL 1
-/* #undef HAVE_BSR64 */
+#endif
