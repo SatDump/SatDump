511da22e3cf3e023ace4674bc7c6ef45051fdbac
Aang23 <qwerty15@gmx.fr>
2022-12-10 23:54:22 +0100

    Oops

--- src-core/products/processor/image_processor.cpp
+++ src-core/products/processor/image_processor.cpp
@@ -75,7 +75,7 @@ namespace satdump
             for (nlohmann::detail::iteration_proxy_value<nlohmann::detail::iter_impl<nlohmann::ordered_json>> compo : instrument_viewer_settings["rgb_composites"].items())
             {
                 if (compo.value().contains("autogen")) // Skip auto-generating if requested
-                    if (ompo.value()["autogen"].get<bool>() == false)
+                    if (compo.value()["autogen"].get<bool>() == false)
                         continue;
 
                 // rgb_presets.push_back({compo.key(), compo.value().get<ImageCompositeCfg>()});
