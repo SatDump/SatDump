fe320422798372f66afcf7eaee90c8bbfc4aa2f3
Aang23 <qwerty15@gmx.fr>
2022-12-15 23:40:35 +0100

    3E MWTS-3 Fixes

--- plugins/fengyun3_support/fengyun3/instruments/mwts3/mwts3_reader.cpp
+++ plugins/fengyun3_support/fengyun3/instruments/mwts3/mwts3_reader.cpp
@@ -10,13 +10,23 @@ namespace fengyun3
             for (int i = 0; i < 18; i++)
                 channels[i].resize(98);
 
-            lines = 0;
+            lines = -1;
         }
 
         MWTS3Reader::~MWTS3Reader()
         {
             for (int i = 0; i < 18; i++)
                 channels[i].clear();
+        }
+
+        uint16_t convert_val(uint16_t v)
+        {
+            bool sign = v >> 15;
+            int val = v & 0b111111111111111;
+            if (sign)
+                return val;
+            else
+                return (65536 / 2) + val;
         }
 
         void MWTS3Reader::work(ccsds::CCSDSPacket &packet)
@@ -28,16 +38,20 @@ namespace fengyun3
 
             if (marker == 1)
             {
+                double currentTime = ccsds::parseCCSDSTimeFullRaw(&packet.payload[2], 10957, 10000, 10000) + 12 * 3600;
+                timestamps.push_back(currentTime);
+                lines++;
+
+                // Make sure we have enough room
+                for (int i = 0; i < 18; i++)
+                    channels[i].resize((lines + 1) * 98);
+
                 int pos = 224 + 144 * 2;
                 for (int i = 0; i < 14; i++)
                 {
                     for (int c = 0; c < 18; c++)
-                        channels[c][lines * 98 + i] = packet.payload[pos + (18 * i + c) * 2 + 0] << 8 | packet.payload[pos + (18 * i + c) * 2 + 1];
+                        channels[c][lines * 98 + i] = convert_val(packet.payload[pos + (18 * i + c) * 2 + 0] << 8 | packet.payload[pos + (18 * i + c) * 2 + 1]);
                 }
-
-                double currentTime = ccsds::parseCCSDSTimeFullRaw(&packet.payload[2], 10957, 10000, 10000) + 12 * 3600;
-                timestamps.push_back(currentTime);
-                lines++;
             }
             else if (marker == 2)
             {
@@ -45,7 +59,7 @@ namespace fengyun3
                 for (int i = 0; i < 28; i++)
                 {
                     for (int c = 0; c < 18; c++)
-                        channels[c][lines * 98 + 14 + i] = packet.payload[pos + (18 * i + c) * 2 + 0] << 8 | packet.payload[pos + (18 * i + c) * 2 + 1];
+                        channels[c][lines * 98 + 14 + i] = convert_val(packet.payload[pos + (18 * i + c) * 2 + 0] << 8 | packet.payload[pos + (18 * i + c) * 2 + 1]);
                 }
             }
             else if (marker == 3)
@@ -54,7 +68,7 @@ namespace fengyun3
                 for (int i = 0; i < 28; i++)
                 {
                     for (int c = 0; c < 18; c++)
-                        channels[c][lines * 98 + 42 + i] = packet.payload[pos + (18 * i + c) * 2 + 0] << 8 | packet.payload[pos + (18 * i + c) * 2 + 1];
+                        channels[c][lines * 98 + 42 + i] = convert_val(packet.payload[pos + (18 * i + c) * 2 + 0] << 8 | packet.payload[pos + (18 * i + c) * 2 + 1]);
                 }
             }
             else if (marker == 4)
@@ -63,13 +77,9 @@ namespace fengyun3
                 for (int i = 0; i < 28; i++)
                 {
                     for (int c = 0; c < 18; c++)
-                        channels[c][lines * 98 + 70 + i] = packet.payload[pos + (18 * i + c) * 2 + 0] << 8 | packet.payload[pos + (18 * i + c) * 2 + 1];
+                        channels[c][lines * 98 + 70 + i] = convert_val(packet.payload[pos + (18 * i + c) * 2 + 0] << 8 | packet.payload[pos + (18 * i + c) * 2 + 1]);
                 }
             }
-
-            // Make sure we have enough room
-            for (int i = 0; i < 18; i++)
-                channels[i].resize((lines + 1) * 98);
         }
 
         image::Image<uint16_t> MWTS3Reader::getChannel(int channel)
