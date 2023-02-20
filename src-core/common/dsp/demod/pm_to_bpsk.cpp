76be3c73d7fbb9dbd51d1c6feb41dde61aafcec2
Aang23 <qwerty15@gmx.fr>
2023-02-20 23:06:33 +0100

    Optimize PM conv

--- src-core/common/dsp/demod/pm_to_bpsk.cpp
+++ src-core/common/dsp/demod/pm_to_bpsk.cpp
@@ -8,10 +8,8 @@ namespace dsp
 {
     PMToBPSK::PMToBPSK(std::shared_ptr<dsp::stream<complex_t>> input, float samplerate, float symbolrate) : Block(input)
     {
-        // Phase and shifting
-        float symbols_per_sample = symbolrate / samplerate;
-        phase_shift = M_TWOPI * -symbols_per_sample;
-        phase_real = 0;
+        phase_delta = complex_t(cos(hz_to_rad(-symbolrate, samplerate)), sin(hz_to_rad(-symbolrate, samplerate)));
+        phase = complex_t(1, 0);
     }
 
     void PMToBPSK::work()
@@ -24,18 +22,9 @@ namespace dsp
         }
 
         for (int i = 0; i < nsamples; i++)
-        {
-            shf = complex_t(fast_cos(phase_real), -fast_sin(phase_real));
+            input_stream->readBuf[i] = complex_t(0, input_stream->readBuf[i].imag);
 
-            output_stream->writeBuf[i] = shf * input_stream->readBuf[i].imag;
-            phase_real += phase_shift;
-
-            // Wrap PM phase
-            while (phase_real < -M_PI)
-                phase_real += M_TWOPI;
-            while (phase_real > M_PI)
-                phase_real -= M_TWOPI;
-        }
+        volk_32fc_s32fc_x2_rotator_32fc((lv_32fc_t *)output_stream->writeBuf, (lv_32fc_t *)input_stream->readBuf, phase_delta, (lv_32fc_t *)&phase, nsamples);
 
         input_stream->flush();
         output_stream->swap(nsamples);
