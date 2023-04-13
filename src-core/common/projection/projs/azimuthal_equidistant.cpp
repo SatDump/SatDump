9842f9f4435a42c2f4d3fd5ad18ee0ab96f65abd
ZbychuButItWasTaken <sztanga.zbyszek@gmail.com>
2023-04-13 21:43:11 +0200

    windows try nb 2

--- src-core/common/projection/projs/azimuthal_equidistant.cpp
+++ src-core/common/projection/projs/azimuthal_equidistant.cpp
@@ -1,7 +1,11 @@
 #include "azimuthal_equidistant.h"
-#define _USE_MATH_DEFINES
 #include <cmath>
 #include <iostream>
+
+#ifndef M_PI
+    #define M_PI 3.14159265358979323846
+#endif
+
 
 namespace geodetic
 {
