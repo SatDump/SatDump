b8149aac8f9844a11a18225efa018c108b5fbfa1
Aang23 <qwerty15@gmx.fr>
2023-05-07 11:31:35 +0200

    Add a MCIR equivalent

--- plugins/analog_support/noaa_apt/module_noaa_apt_decoder.cpp
+++ plugins/analog_support/noaa_apt/module_noaa_apt_decoder.cpp
@@ -56,7 +56,10 @@ namespace noaa_apt
 
             wav::FileMetadata md = wav::tryParseFilenameMetadata(d_input_file, true);
             if (md.timestamp != 0 && (d_parameters.contains("start_timestamp") ? d_parameters["start_timestamp"] == -1 : 1))
-                d_parameters["start_timestamp"] = md.timestamp;
+            {
+                d_parameters["start_timestamp"] = md.timestamp-3600;
+                logger->trace("Has timestamp {:d}", md.timestamp);
+            }
 
             data_in = std::ifstream(d_input_file, std::ios::binary);
         }
