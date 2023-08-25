0727ae6e790e40ddd6ac7f71b8c6d36b900b4ed6
Jamie Vital <jvital2013@gmail.com>
2023-08-24 23:18:43 -0400

    Fix macOS again

--- macOS/bundle.sh
+++ macOS/bundle.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
 echo "Making app shell..." 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR/../build
 mkdir -p MacApp/SatDump.app/Contents/MacOS
 mkdir -p MacApp/SatDump.app/Contents/Resources/plugins
-cp -r ../resources MacApp/SatDump.app/Contents/Resources/resources
-cp -r ../pipelines MacApp/SatDump.app/Contents/Resources/pipelines
-cp ../satdump_cfg.json MacApp/SatDump.app/Contents/Resources
-cp $SCRIPT_DIR/Info.plist MacApp/SatDump.app/Contents
+cp -r $GITHUB_WORKSPACE/resources MacApp/SatDump.app/Contents/Resources/resources
+cp -r $GITHUB_WORKSPACE/pipelines MacApp/SatDump.app/Contents/Resources/pipelines
+cp $GITHUB_WORKSPACE/satdump_cfg.json MacApp/SatDump.app/Contents/Resources
+cp $GITHUB_WORKSPACE/macOS/Info.plist MacApp/SatDump.app/Contents
 
 echo "Creating app icon..." 
 mkdir macOSIcon.iconset
-sips -z 16 16     ../icon.png --out macOSIcon.iconset/icon_16x16.png
-sips -z 32 32     ../icon.png --out macOSIcon.iconset/icon_16x16@2x.png
-sips -z 32 32     ../icon.png --out macOSIcon.iconset/icon_32x32.png
-sips -z 64 64     ../icon.png --out macOSIcon.iconset/icon_32x32@2x.png
-sips -z 128 128   ../icon.png --out macOSIcon.iconset/icon_128x128.png
-sips -z 256 256   ../icon.png --out macOSIcon.iconset/icon_128x128@2x.png
-sips -z 256 256   ../icon.png --out macOSIcon.iconset/icon_256x256.png
-sips -z 512 512   ../icon.png --out macOSIcon.iconset/icon_256x256@2x.png
-sips -z 512 512   ../icon.png --out macOSIcon.iconset/icon_512x512.png
-sips -z 1024 1024 ../icon.png --out macOSIcon.iconset/icon_512x512@2x.png
+sips -z 16 16     $GITHUB_WORKSPACE/icon.png --out macOSIcon.iconset/icon_16x16.png
+sips -z 32 32     $GITHUB_WORKSPACE/icon.png --out macOSIcon.iconset/icon_16x16@2x.png
+sips -z 32 32     $GITHUB_WORKSPACE/icon.png --out macOSIcon.iconset/icon_32x32.png
+sips -z 64 64     $GITHUB_WORKSPACE/icon.png --out macOSIcon.iconset/icon_32x32@2x.png
+sips -z 128 128   $GITHUB_WORKSPACE/icon.png --out macOSIcon.iconset/icon_128x128.png
+sips -z 256 256   $GITHUB_WORKSPACE/icon.png --out macOSIcon.iconset/icon_128x128@2x.png
+sips -z 256 256   $GITHUB_WORKSPACE/icon.png --out macOSIcon.iconset/icon_256x256.png
+sips -z 512 512   $GITHUB_WORKSPACE/icon.png --out macOSIcon.iconset/icon_256x256@2x.png
+sips -z 512 512   $GITHUB_WORKSPACE/icon.png --out macOSIcon.iconset/icon_512x512.png
+sips -z 1024 1024 $GITHUB_WORKSPACE/icon.png --out macOSIcon.iconset/icon_512x512@2x.png
 iconutil -c icns -o MacApp/SatDump.app/Contents/Resources/icon.icns macOSIcon.iconset
 rm -rf macOSIcon.iconset
 
