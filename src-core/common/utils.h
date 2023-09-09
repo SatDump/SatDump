6f0c5a59d30f29e282be76ed270359702585c33c
Aang23 <qwerty15@gmx.fr>
2023-09-09 22:35:30 +0200

    Fixup

--- src-core/common/utils.h
+++ src-core/common/utils.h
@@ -158,6 +158,38 @@ std::vector<T> oversample_vector(std::vector<T> data, int oversampling)
     return r;
 }
 
+template <typename... T>
+std::string svformat(const char *fmt, T &&...args)
+{
+    // Allocate a buffer on the stack that's big enough for us almost
+    // all the time.
+    size_t size = 1024;
+    std::vector<char> buf;
+    buf.resize(size);
+
+    // Try to vsnprintf into our buffer.
+    size_t needed = snprintf((char *)&buf[0], size, fmt, args...);
+    // NB. On Windows, vsnprintf returns -1 if the string didn't fit the
+    // buffer.  On Linux & OSX, it returns the length it would have needed.
+
+    if (needed <= size)
+    {
+        // It fit fine the first time, we're done.
+        return std::string(&buf[0]);
+    }
+    else
+    {
+        // vsnprintf reported that it wanted to write more characters
+        // than we allotted.  So do a malloc of the right size and try again.
+        // This doesn't happen very often if we chose our initial size
+        // well.
+        size = needed;
+        buf.resize(size);
+        needed = snprintf((char *)&buf[0], size, fmt, args...);
+        return std::string(&buf[0]);
+    }
+}
+
 std::string loadFileToString(std::string path);
 std::string ws2s(const std::wstring &wstr);
 std::wstring s2ws(const std::string &str);
