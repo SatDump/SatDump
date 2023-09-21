c1e28d313de6e048abaa6b516652f91e86862d81
Aang23 <qwerty15@gmx.fr>
2023-09-21 16:58:04 +0200

    Warnings

--- src-core/common/image/composite.cpp
+++ src-core/common/image/composite.cpp
@@ -334,7 +334,7 @@ namespace image
             { get_channel_vals(channelValues, inputChannels, channelNumbers, f, y, x); };
             lua["get_calibrated_image"] = [img_pro](int ch, std::string type, float min, float max)
             { 
-                satdump::ImageProducts::calib_vtype_t ctype;
+                satdump::ImageProducts::calib_vtype_t ctype = satdump::ImageProducts::CALIB_VTYPE_AUTO;
 
                 if(type == "auto")
                     ctype = satdump::ImageProducts::CALIB_VTYPE_AUTO;
