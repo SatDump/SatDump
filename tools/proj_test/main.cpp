91f063e436889e45a01befc78e9c0160faee53c8
Aang23 <qwerty15@gmx.fr>
2023-10-14 19:07:52 +0200

    Retune FY-3C Instruments

--- tools/proj_test/main.cpp
+++ tools/proj_test/main.cpp
@@ -56,7 +56,7 @@ int main(int /*argc*/, char *argv[])
     printf("\n%s\n", img_pro.contents.dump(4).c_str());
 
     satdump::ImageCompositeCfg rgb_cfg;
-    rgb_cfg.equation = "1-ch37"; //"(ch7421+ch7422+ch7423+ch7242)/4";
+    rgb_cfg.equation = "1-ch1"; //"(ch7421+ch7422+ch7423+ch7242)/4";
     //    rgb_cfg.equation = "1-ch37";
     // rgb_cfg.equation = "1-ch33,1-ch34,1-ch35"; //"(ch3 * 0.4 + ch2 * 0.6) * 2.2 - 0.15, ch2 * 2.2 - 0.15, ch1 * 2.2 - 0.15";
     rgb_cfg.individual_equalize = true;
@@ -81,8 +81,8 @@ int main(int /*argc*/, char *argv[])
     operation_t.ground_control_points = satdump::gcp_compute::compute_gcps(proj_cfg,
                                                                            operation_t.input_image.width(),
                                                                            operation_t.input_image.height());
-    operation_t.output_width = 2048 * 2;  // 10;  // 10;
-    operation_t.output_height = 1024 * 2; // 10; // 10;
+    operation_t.output_width = 2048 * 10;  // 10;
+    operation_t.output_height = 1024 * 10; // 10;
     operation_t.output_rgba = true;
 
     auto warp_result = satdump::warp::performSmartWarp(operation_t);
