3905a399216d76f8d85293b7b70103f31ff58a7c
Aang23 <qwerty15@gmx.fr>
2023-10-16 16:04:37 +0200

    nat2pro fixes for MetOp AVHRR

--- tools/nat2pro/avhrr_nat.cpp
+++ tools/nat2pro/avhrr_nat.cpp
@@ -73,6 +73,8 @@ void decodeAVHRRNat(std::vector<uint8_t> nat_file, std::string pro_output_file)
                 nat_file[current_ptr + 22 + 0] << 8 |
                 nat_file[current_ptr + 22 + 1];
 
+            bool ch3a = nat_file[current_ptr + 26580 + 1] & 1;
+
             number_of_lines++;
             image_width = number_of_samples;
 
@@ -87,7 +89,7 @@ void decodeAVHRRNat(std::vector<uint8_t> nat_file, std::string pro_output_file)
                 if (c == 1)
                     c2 = 1;
                 if (c == 2)
-                    c2 = 2;
+                    c2 = ch3a ? 2 : 3;
                 if (c == 3)
                     c2 = 4;
                 if (c == 4)
@@ -100,46 +102,46 @@ void decodeAVHRRNat(std::vector<uint8_t> nat_file, std::string pro_output_file)
                     avhrr_data[c2][(number_of_lines - 1) * image_width + i] = val + 32768;
                 }
             }
-        }
 
-        if ((number_of_lines - 1) % 50 == 0)
-        {
-            double lat_first = get_deg_latlon(&nat_file[current_ptr + 20538 + 0]);
-            double lon_first = get_deg_latlon(&nat_file[current_ptr + 20538 + 4]);
-
-            double lat_last = get_deg_latlon(&nat_file[current_ptr + 20546 + 0]);
-            double lon_last = get_deg_latlon(&nat_file[current_ptr + 20546 + 4]);
-
-            gcps_all[all_gcps]["x"] = 0;
-            gcps_all[all_gcps]["y"] = number_of_lines - 1;
-            gcps_all[all_gcps]["lat"] = lat_first;
-            gcps_all[all_gcps]["lon"] = lon_first;
-            all_gcps++;
-
-            gcps_all[all_gcps]["x"] = image_width - 1;
-            gcps_all[all_gcps]["y"] = number_of_lines - 1;
-            gcps_all[all_gcps]["lat"] = lat_last;
-            gcps_all[all_gcps]["lon"] = lon_last;
-            all_gcps++;
-
-            for (int i = 0; i < 103; i += 2)
+            if ((number_of_lines - 1) % 50 == 0)
             {
-                int px = 4 + i * 20;
-                if (px >= 2048)
-                    continue;
-                double lat = get_deg_latlon(&nat_file[current_ptr + 21380 + i * 8 + 0]);
-                double lon = get_deg_latlon(&nat_file[current_ptr + 21380 + i * 8 + 4]);
-                gcps_all[all_gcps]["x"] = px;
+                double lat_first = get_deg_latlon(&nat_file[current_ptr + 20538 + 0]);
+                double lon_first = get_deg_latlon(&nat_file[current_ptr + 20538 + 4]);
+
+                double lat_last = get_deg_latlon(&nat_file[current_ptr + 20546 + 0]);
+                double lon_last = get_deg_latlon(&nat_file[current_ptr + 20546 + 4]);
+
+                gcps_all[all_gcps]["x"] = 0;
                 gcps_all[all_gcps]["y"] = number_of_lines - 1;
-                gcps_all[all_gcps]["lat"] = lat;
-                gcps_all[all_gcps]["lon"] = lon;
+                gcps_all[all_gcps]["lat"] = lat_first;
+                gcps_all[all_gcps]["lon"] = lon_first;
                 all_gcps++;
 
-                logger->trace("%f %f", lon, lat);
-            }
+                gcps_all[all_gcps]["x"] = image_width - 1;
+                gcps_all[all_gcps]["y"] = number_of_lines - 1;
+                gcps_all[all_gcps]["lat"] = lat_last;
+                gcps_all[all_gcps]["lon"] = lon_last;
+                all_gcps++;
 
-            logger->info("%d %d %d %f %f %f %f", (int)main_header.record_class, (int)main_header.record_size, (int)main_header.record_subclass_version,
-                         lon_first, lat_first, lon_last, lat_last);
+                for (int i = 0; i < 103; i += 2)
+                {
+                    int px = 4 + i * 20;
+                    if (px >= 2048)
+                        continue;
+                    double lat = get_deg_latlon(&nat_file[current_ptr + 21380 + i * 8 + 0]);
+                    double lon = get_deg_latlon(&nat_file[current_ptr + 21380 + i * 8 + 4]);
+                    gcps_all[all_gcps]["x"] = px;
+                    gcps_all[all_gcps]["y"] = number_of_lines - 1;
+                    gcps_all[all_gcps]["lat"] = lat;
+                    gcps_all[all_gcps]["lon"] = lon;
+                    all_gcps++;
+
+                    // logger->trace("%f %f", lon, lat);
+                }
+
+                logger->info("%d %d %d %f %f %f %f %d", (int)main_header.record_class, (int)main_header.record_size, (int)main_header.record_subclass_version,
+                             lon_first, lat_first, lon_last, lat_last, ch3a);
+            }
         }
 
         current_ptr += main_header.record_size;
