b3dad797bba6cb6cc2cb83f828b88a3ec778684f
Jamie Vital <jvital2013@gmail.com>
2023-11-04 15:01:06 -0400

    Add BUILD_GLES and OpenGL fallbacks

--- src-ui/main.cpp
+++ src-ui/main.cpp
@@ -73,22 +73,24 @@ int main(int argc, char *argv[])
 #endif
 
     // Decide GL+GLSL versions
+    const char* OPENGL_VERSIONS_GLSL[] = { "#version 100", "#version 120", "#version 130", "#version 150" };
+    int selected_glsl;
 #if defined(IMGUI_IMPL_OPENGL_ES2)
     // GL ES 2.0 + GLSL 100
-    const char* glsl_version = "#version 100";
+    selected_glsl = 0;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
 #elif defined(__APPLE__)
     // GL 3.2 + GLSL 150
-    const char* glsl_version = "#version 150";
+    selected_glsl = 3;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
 #else
     // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
+    selected_glsl = 2;
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
@@ -98,8 +100,15 @@ int main(int argc, char *argv[])
     GLFWwindow* window = glfwCreateWindow(1000, 600, std::string("SatDump v" + (std::string)SATDUMP_VERSION).c_str(), NULL, NULL);
     if (window == nullptr)
     {
-        logger->critical("Could not init GLFW Window");
-        exit(1);
+        logger->warn("Could not init GLFW Window; falling back to OpenGL 2.1...");
+        glfwDefaultWindowHints();
+        selected_glsl = 1;
+        window = glfwCreateWindow(1000, 600, std::string("SatDump v" + (std::string)SATDUMP_VERSION).c_str(), NULL, NULL);
+        if (window == nullptr)
+        {
+            logger->critical("Could not init GLFW Window! Exiting");
+            exit(1);
+        }
     }
 
     glfwMakeContextCurrent(window);
@@ -117,7 +126,7 @@ int main(int argc, char *argv[])
 
     // Setup Platform/Renderer bindings
     ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init(glsl_version);
+    ImGui_ImplOpenGL3_Init(OPENGL_VERSIONS_GLSL[selected_glsl]);
 
     // Setup Icon
     GLFWimage img;
