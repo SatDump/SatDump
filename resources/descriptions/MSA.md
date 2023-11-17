e5f983fefe5603af261843742fa5314a0612556d
Jacopo Cassinis <12469744+RAD750@users.noreply.github.com>
2023-11-17 09:24:11 +0100

    Added good NDVI

--- resources/descriptions/MSA.md
+++ resources/descriptions/MSA.md
@@ -1,10 +1,8 @@
-# Cloud Underlay
+# MSA
 
-This enhancement strips only the clouds from the satellite image, and applies them as a layer over a predefined map. This can help to differentiate some cloud types from land in single-band visible imagery, typical of APT transmissions.
+This enhancement strips only the clouds from the satellite image, and applies them as a layer over a predefined map. This can help to differentiate cloud types from land in single-band imagery, typical of APT transmissions.
 
-Compared to the Cloud Underlay, it tries to preserve more cloud data.
-
-Compared to MCIR, it uses a visible channel (channels 1 or 2 on AVHRR).
+Compared to MCIR, it also uses a visible channel.
 
 ### Appearance
 
@@ -13,9 +11,8 @@ Land and sea appear as specified by the map used (by default, a physical map).
 
 ### Intended usage
 
-Aesthetical reasons, cloud analysis on APT at daytime.
+Cloud analysis on APT.
 
 ### Limitations
 
 No cloud top temperatures.
-Not usable at night.
--- resources/scripted_compos/ndvi.lua
+++ resources/scripted_compos/ndvi.lua
@@ -1,31 +1,45 @@
 -- NDVI
 
 function init()
+    --create an empty image for the LUT
     img_lut = image8.new()
+    --load the LUT
     img_lut:load_png(get_resource_path("lut/NDVI.png"))
+    --return 3 channels, RGB
     return 3
 end
 
 function process()
     for x = 0, rgb_output:width() - 1, 1 do
         for y = 0, rgb_output:height() - 1, 1 do
+
+            --get channels from satdump.json
+
             local cch2 = get_calibrated_value(0, x, y, true)
             local cch1 = get_calibrated_value(1, x, y, true)
+            
 
-
-
+            --perform NDVI
             local ndvi = (cch2-cch1)/(cch2+cch1)
 
-            local ndvi_lutval = ((ndvi-(-1))*256)/(1-(-1))+0 
-            
-            local lut_result[0] = img_lut:get(0 * img_lut:height() * img_lut:width() + ndvi_lutval) / 255.0
-            local lut_result[1] = img_lut:get(1 * img_lut:height() * img_lut:width() + ndvi_lutval) / 255.0
-            local lut_result[2] = img_lut:get(2 * img_lut:height() * img_lut:width() + ndvi_lutval) / 255.0
+            --range convert from -1 -> 1 to 0 -> 256
+            local ndvi_lutval = 256 - ((ndvi-(-1))*256)/(1-(-1))+0
 
-            for c = 0, 2, 1 do
-                set_img_out(c, x, y, lut_result[c])
-            end
+            --don't forget to create a table!
+            local lut_result = {}
+        
+            --send the ndvi_lutval to the LUT, retrieve it back in the table as 3 R,G,B channels
+            lut_result[0] = img_lut:get(0 * img_lut:height() * img_lut:width() + ndvi_lutval * img_lut:width() + ndvi_lutval) / 255.0
+            lut_result[1] = img_lut:get(1 * img_lut:height() * img_lut:width() + ndvi_lutval * img_lut:width() + ndvi_lutval) / 255.0
+            lut_result[2] = img_lut:get(2 * img_lut:height() * img_lut:width() + ndvi_lutval * img_lut:width() + ndvi_lutval) / 255.0
+            
+            --return RGB 0=R 1=G 2=B
+            set_img_out(0, x, y, lut_result[0])
+            set_img_out(1, x, y, lut_result[1])
+            set_img_out(2, x, y, lut_result[2])
+            
         end
+        --set the progress bar accordingly
         set_progress(x, rgb_output:width())
     end
 end
