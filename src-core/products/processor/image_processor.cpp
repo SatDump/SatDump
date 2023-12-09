6c17878d27eab0e914e447a4bdfbaec5022c1ff8
Jamie Vital <jvital2013@gmail.com>
2023-12-08 20:34:30 -0500

    Remove unused variable

--- src-core/products/processor/image_processor.cpp
+++ src-core/products/processor/image_processor.cpp
@@ -104,7 +104,6 @@ namespace satdump
                     {
                         corrected_stuff.resize(rgb_image.width());
                         bool success = false;
-                        image::Image<uint16_t> cor;
                         rgb_image_corr = perform_geometric_correction(*img_products, rgb_image, success, corrected_stuff.data());
                         if (!success)
                         {
