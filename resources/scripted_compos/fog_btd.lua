722f0ad516f1d7348eb651db91fafc0d0159191e
Marhmarchi <58359447+Marhmarchi@users.noreply.github.com>
2024-01-11 18:42:45 -0300

    Update fog_btd.lua

--- resources/scripted_compos/fog_btd.lua
+++ resources/scripted_compos/fog_btd.lua
@@ -1,10 +1,6 @@
 -- 10.8, 3.9 um fog BTD
 
 function init()
-    --create an empty image for the LUT
-    img_lut = image8.new()
-    --load the LUT
-    img_lut:load_png(get_resource_path("lut/fog_btd.png"))
     --return 3 channels, RGB
     return 3
 end
@@ -14,40 +10,17 @@ function process()
         for y = 0, rgb_output:height() - 1, 1 do
 
             --get channels
-
+            
             get_channel_values(x, y)
-            local cch3b = get_channel_value(0) * 1000
-            local cch4 = get_channel_value(1) * 1000
+            local cch3b = get_channel_value(0) * 500
+            local cch4 = get_channel_value(1) * 500
             local btd = cch4 - cch3b
-            print(btd)
-
-            --range convert from -1 -> 1 to 0 -> 256
-            local btd_lutval = (((btd-(-15))*256) / (15-(-15)))
-
-            --sanity checks (noise)
-            if btd_lutval < 0 then
-                btd_lutval = 0
-            end
             
-            if btd_lutval > 255 then
-                btd_lutval = 255
-            end
-
-
-            --don't forget to create a table!
-            local lut_result = {}
-    
-            --send the ndvi_lutval to the LUT, retrieve it back in the table as 3 R,G,B channels
-            lut_result[0] = img_lut:get(0 * img_lut:height() * img_lut:width() + btd_lutval * img_lut:width() + btd_lutval) / 255.0
-            lut_result[1] = img_lut:get(1 * img_lut:height() * img_lut:width() + btd_lutval * img_lut:width() + btd_lutval) / 255.0
-            lut_result[2] = img_lut:get(2 * img_lut:height() * img_lut:width() + btd_lutval * img_lut:width() + btd_lutval) / 255.0
-            
-            
-
             --return RGB 0=R 1=G 2=B
-            set_img_out(0, x, y, lut_result[0])
-            set_img_out(1, x, y, lut_result[1])
-            set_img_out(2, x, y, lut_result[2])
+            
+            set_img_out(0, x, y, btd)
+            set_img_out(1, x, y, btd)
+            set_img_out(2, x, y, btd)
             
         end
         --set the progress bar accordingly
