79586af7493bcde6007a8bc508a42ca97ba4beb9
Jamie Vital <jvital2013@gmail.com>
2024-02-28 19:52:09 -0500

    Fix save dialog breaking when saving as .tiff (two fs)

--- src-interface/viewer/image_handler.cpp
+++ src-interface/viewer/image_handler.cpp
@@ -711,17 +711,14 @@ namespace satdump
             if (ImGui::CollapsingHeader("Projection"))
             {
                 ImGui::BeginGroup();
-                ImGui::Checkbox("Old Algorithm", &projection_use_old_algo);
-                ImGui::SameLine();
                 if (!canBeProjected())
                     style::beginDisabled();
                 if (ImGui::Button("Add to Projections"))
                     addCurrentToProjections();
+                proj_notif.draw();
+                ImGui::Checkbox("Old Algorithm", &projection_use_old_algo);
                 if (!canBeProjected())
                     style::endDisabled();
-
-                ImGui::SameLine();
-                proj_notif.draw();
 
                 ImGui::EndGroup();
                 if (ImGui::IsItemHovered(ImGuiHoveredFlags_AllowWhenDisabled) && !canBeProjected())
