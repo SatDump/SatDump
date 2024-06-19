8ff15840942d64060bf0f54597707b58116dd562
Jamie Vital <JVital2013@gmail.com>
2024-06-19 16:52:27 -0400

    Update Get-NSIS-LongStrings.ps1

--- windows/Get-NSIS-LongStrings.ps1
+++ windows/Get-NSIS-LongStrings.ps1
@@ -5,6 +5,8 @@
 }
 
 cd "$(Split-Path -Parent $MyInvocation.MyCommand.Path)\..\vcpkg"
+rm -force nsis-3.10-strlen_8192.zip -erroraction silentlycontinue
+rm -recurse -force nsis-3.10-strlen_8192 -erroraction silentlycontinue
 Invoke-WebRequest https://downloads.sourceforge.net/project/nsis/NSIS%203/3.10/nsis-3.10-strlen_8192.zip -OutFile nsis-3.10-strlen_8192.zip -UserAgent "Wget/1.21.1" | Out-Null
 if(-not $(Test-Path .\nsis-3.10-strlen_8192.zip))
 {
