97d00e5d07e19c6f1cf11464eb16fd187b88e260
Jamie Vital <jvital2013@gmail.com>
2024-07-13 11:35:18 -0400

    Allow adding items in json editor

--- src-core/common/widgets/json_editor.cpp
+++ src-core/common/widgets/json_editor.cpp
@@ -93,9 +93,86 @@ namespace widgets
 			ImGui::PopID();
 		}
 
-		if (ImGui::Button("Add...##NewItem"))
+		if (ImGui::Button("Add..."))
+			ImGui::OpenPopup("Add Item");
+
+		ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+		ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+		if (ImGui::BeginPopupModal("Add Item", NULL, ImGuiWindowFlags_AlwaysAutoResize))
 		{
-			//TODO
+			static std::string new_key = "new_item";
+			if (json.is_object())
+			{
+				ImGui::TextUnformatted("Key:");
+				ImGui::SameLine();
+				ImGui::InputText("##label", &new_key, ImGuiInputTextFlags_AutoSelectAll);
+			}
+
+			static int selected = 0;
+			ImGui::SameLine();
+			ImGui::TextUnformatted("Type:");
+			ImGui::SameLine();
+			ImGui::Combo("##Type", &selected, "Object\0Array\0String\0Integer\0Double\0Boolean\0\0");
+			ImGuiStyle& style = ImGui::GetStyle();
+			ImGui::SetCursorPos({ (ImGui::GetContentRegionAvail().x / 2) - ((ImGui::CalcTextSize("Add").x + ImGui::CalcTextSize("Cancel").x +
+				style.ItemSpacing.x + style.FramePadding.x * 4) / 2), ImGui::GetCursorPosY() + 5.0f * ui_scale});
+
+			if (ImGui::Button("Add"))
+			{
+				if (json.is_object())
+				{
+					switch (selected)
+					{
+						case 0:
+							json[new_key] = nlohmann::json::object();
+							break;
+						case 1:
+							json[new_key] = nlohmann::json::array();
+							break;
+						case 2:
+							json[new_key] = "";
+							break;
+						case 3:
+							json[new_key] = 0;
+							break;
+						case 4:
+							json[new_key] = 0.0;
+							break;
+						case 5:
+							json[new_key] = false;
+					}
+				}
+				else
+				{
+					switch (selected)
+					{
+						case 0:
+							json.push_back(nlohmann::json::object());
+							break;
+						case 1:
+							json.push_back(nlohmann::json::array());
+							break;
+						case 2:
+							json.push_back("");
+							break;
+						case 3:
+							json.push_back(0);
+							break;
+						case 4:
+							json.push_back(0.0);
+							break;
+						case 5:
+							json.push_back(false);
+					}
+				}
+				ImGui::CloseCurrentPopup();
+			}
+			ImGui::SameLine();
+			if (ImGui::Button("Cancel"))
+			{
+				ImGui::CloseCurrentPopup();
+			}
+			ImGui::EndPopup();
 		}
 	}
 }
