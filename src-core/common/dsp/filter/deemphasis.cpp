9d26c284952fa5f9c8cd73a65e03ad44ebea7ed8
Marhmarchi <58359447+Marhmarchi@users.noreply.github.com>
2024-08-17 15:31:05 -0300

    DeEmph fix & cleanup 2

--- src-core/common/dsp/filter/deemphasis.cpp
+++ src-core/common/dsp/filter/deemphasis.cpp
@@ -1,5 +1,6 @@
 #include "common/dsp/filter/deemphasis.h"
 #include "common/dsp/buffer.h"
+#include <complex.h>
 #include <memory>
 #include <type_traits>
 
@@ -41,38 +42,42 @@ namespace dsp
         	int consumed = 0;
 
 
-		for (int i = 0; (i + nsamples) < in_buffer; i += nsamples)
+		for (int i = 0; (i + d_nsamples) < in_buffer; i += d_nsamples)
 		{
 			T input = Block<T, T>::input_stream->readBuf[i];
 			T output = Block<T, T>::output_stream->readBuf[i];
 
-			consumed += nsamples;
+			consumed += d_nsamples;
 
 			//float dt = 1.0f / quad_sampl_rate;
 			//alpha = dt / (tau + dt);
 
 			if constexpr (std::is_same_v<T, float>)
 			{
+				std::vector<float> input;
+				std::vector<float> output;
 				output[0] = (alpha * input[0]) + ((1 - alpha) * lastSamp);
-				for (int j = 1; j < nsamples; j++)
+				for (int j = 1; j < d_nsamples; j++)
 				{
 					output[j] = (alpha * input[j]) + ((1 - alpha) * output[j -1]);
 				}
-				lastSamp = output[nsamples - 1];
+				lastSamp = output[d_nsamples - 1];
 			}
 
 			if constexpr (std::is_same_v<T, complex_t>)
 			{
+				std::vector<complex_t> input;
+				std::vector<complex_t> output;
 				output[0].imag = (alpha * input[0].imag) + ((1 - alpha) * lastSampImag);
 				output[0].real = (alpha * input[0].real) + ((1 - alpha) * lastSampReal);
-				for (int j = 1; j < nsamples; j++)
+				for (int j = 1; j < d_nsamples; j++)
 				{
 					output[j].imag = (alpha * input[j].imag) + ((1 - alpha) * output[j - 1].imag);
 					output[j].real = (alpha * input[j].real) + ((1 - alpha) * output[j - 1].real);
 				}
 
-				lastSampImag = output[nsamples - 1].imag;
-				lastSampReal = output[nsamples - 1].real;
+				lastSampImag = output[d_nsamples - 1].imag;
+				lastSampReal = output[d_nsamples - 1].real;
 
 			}
 
@@ -81,10 +86,13 @@ namespace dsp
 
 		Block<T, T>::input_stream->flush();
 		//Block<T, T>::output_stream->swap(nsamples);
+		Block<T, T>::output_stream->swap(consumed);
 
 		memmove(&buffer[0], &buffer[consumed], (in_buffer - consumed) * sizeof(T));
         	in_buffer -= consumed;
 
-
 	}
+
+	template class DeEmphasisBlock<complex_t>;
+	template class DeEmphasisBlock<float>;
 }
--- src-core/common/dsp/filter/deemphasis.h
+++ src-core/common/dsp/filter/deemphasis.h
@@ -11,25 +11,17 @@ namespace dsp
 	{
 	private:
 		T *buffer;
-		//float **taps;
-		//float *ataps;
-		//float *btaps;
 		float lastSamp;
 		float lastSampImag;
 		float lastSampReal;
-		//int ntaps;
 		double tau;
 		void work();
 
 		int in_buffer = 0;
-		//int d_nsamples = 0;
+		int d_nsamples = 0;
 
 		float alpha;
 		double quad_sampl_rate = 0;
-
-    		/* De-emph IIR filter taps */
-    		//std::vector<double> d_fftaps;  /*! Feed forward taps. */
-    		//std::vector<double> d_fbtaps;  /*! Feed back taps. */
 
 	public:
 		DeEmphasisBlock(std::shared_ptr<dsp::stream<T>> input, double quad_sampl_rate, double tau);
