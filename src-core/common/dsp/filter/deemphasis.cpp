8e7ec9c53361b47d19eef245f5a0f0874f2ec38b
Marhmarchi <58359447+Marhmarchi@users.noreply.github.com>
2024-08-17 14:24:39 -0300

    DeEmph work

--- src-core/common/dsp/filter/deemphasis.cpp
+++ src-core/common/dsp/filter/deemphasis.cpp
@@ -8,7 +8,7 @@
 namespace dsp
 {
 	template<typename T>
-	DeEmphasisBlock<T>::DeEmphasisBlock(std::shared_ptr<dsp::stream<T>> input, double quad_sampl_rate, double tau, int ntaps) : Block<T, T>(input)
+	DeEmphasisBlock<T>::DeEmphasisBlock(std::shared_ptr<dsp::stream<T>> input, double quad_sampl_rate, double tau) : Block<T, T>(input)
 	{
 		float dt = 1.0f / quad_sampl_rate;
 		alpha = dt / (tau + dt);
@@ -32,13 +32,66 @@ namespace dsp
         		return;
         	}
 
+		for (int i = 0; i < nsamples; i++)
+		{
+			T input = Block<T, T>::input_stream->readBuf[i];
+			T output = Block<T, T>::output_stream->readBuf[i];
+
+			//float dt = 1.0f / quad_sampl_rate;
+			//alpha = dt / (tau + dt);
+
+			if constexpr (std::is_same_v<T, float>)
+			{
+				output[0] = (alpha * input[0]) + ((1 - alpha) * lastSamp);
+				for (int j = 1; j < nsamples; j++)
+				{
+					output[j] = (alpha * input[j]) + ((1 - alpha) * output[j -1]);
+				}
+				lastSamp = output[nsamples - 1];
+			}
+
+			if constexpr (std::is_same_v<T, complex_t>)
+			{
+				output[0].imag = (alpha * input[0].imag) + ((1 - alpha) * lastSampImag);
+				output[0].real = (alpha * input[0].real) + ((1 - alpha) * lastSampReal);
+				for (int j = 1; j < nsamples; j++)
+				{
+					output[j].imag = (alpha * input[j].imag) + ((1 - alpha) * output[j - 1].imag);
+					output[j].real = (alpha * input[j].real) + ((1 - alpha) * output[j - 1].real);
+				}
+
+				lastSampImag = output[nsamples - 1].imag;
+				lastSampReal = output[nsamples - 1].real;
+
+			}
+
+			Block<T, T>::output_stream->writeBuf[i] = output;
+		}
+
+		Block<T, T>::input_stream->flush();
+		Block<T, T>::output_stream->swap(nsamples);
+	}
+}
+
+
+
+
+
+
+		/*
         	memcpy(&buffer[ntaps], Block<T, T>::input_stream->readBuf, nsamples * sizeof(T));
         	Block<T, T>::input_stream->flush();
 
+
+
+
+		/*
 		if constexpr (std::is_same_v<T, float>)
 		{
 			for (int i = 0; i < nsamples; i++)
 			{
+
+
         			// copied from fm_emph.py in gr-analog
         			double  w_c;    // Digital corner frequency
         			double  w_ca;   // Prewarped analog corner frequency
--- src-core/common/dsp/filter/deemphasis.h
+++ src-core/common/dsp/filter/deemphasis.h
@@ -11,25 +11,28 @@ namespace dsp
 	{
 	private:
 		T *buffer;
-		float **taps;
-		float *ataps;
-		float *btaps;
-		int ntaps;
+		//float **taps;
+		//float *ataps;
+		//float *btaps;
+		float lastSamp;
+		float lastSampImag;
+		float lastSampReal;
+		//int ntaps;
 		double tau;
 		void work();
 
-		int in_buffer = 0;
-		int d_nsamples = 0;
+		//int in_buffer = 0;
+		//int d_nsamples = 0;
 
-		float alpha = 1.0f;
+		float alpha;
 		double quad_sampl_rate = 0;
 
     		/* De-emph IIR filter taps */
-    		std::vector<double> d_fftaps;  /*! Feed forward taps. */
-    		std::vector<double> d_fbtaps;  /*! Feed back taps. */
+    		//std::vector<double> d_fftaps;  /*! Feed forward taps. */
+    		//std::vector<double> d_fbtaps;  /*! Feed back taps. */
 
 	public:
-		DeEmphasisBlock(std::shared_ptr<dsp::stream<T>> input, double quad_sampl_rate, double tau, int ntaps);
+		DeEmphasisBlock(std::shared_ptr<dsp::stream<T>> input, double quad_sampl_rate, double tau);
 		~DeEmphasisBlock();
 	};
 }
