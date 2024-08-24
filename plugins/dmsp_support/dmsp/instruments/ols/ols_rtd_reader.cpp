8a1335cea5aa50af9401b3582135767b3969fce4
sgcderek <76875958+sgcderek@users.noreply.github.com>
2024-08-25 00:23:27 +0200

    Better OLS RTD line frame sync

--- plugins/dmsp_support/dmsp/instruments/ols/ols_rtd_reader.cpp
+++ plugins/dmsp_support/dmsp/instruments/ols/ols_rtd_reader.cpp
@@ -23,18 +23,29 @@ namespace dmsp
 
         void OLSRTDReader::work(uint8_t *rtd_frame, uint8_t *rtd_words)
         {
-            if (rtd_words[1] == 0xFB &&
-                rtd_words[2] == 0x07 &&
-                rtd_words[3] == 0xFB &&
-                rtd_words[4] == 0x07 &&
-                rtd_words[5] == 0xFB &&
-                rtd_words[6] == 0x07 &&
-                rtd_words[7] == 0xFB &&
-                rtd_words[8] == 0x07 &&
-                rtd_words[9] == 0xFB &&
-                rtd_words[10] == 0x07 &&
-                rtd_words[11] == 0xFB &&
-                rtd_words[12] == 0x07)
+            int alarm_match = 0;
+
+            for (int i = 1; i < 13; i++)
+            {
+                // Determine odd/even alarm code for word
+                int alarm_code = ((i & 1) == 0) ? 0x07 : 0xFB;
+
+                // Read word to check against alarm code
+                int check_word = rtd_words[i];
+
+                // Compare each bit and count matches
+                for (int j = 1; j < 9; j++)
+                {
+                    if ((check_word & 0x01) == (alarm_code & 0x01))
+                    {
+                        alarm_match++;
+                    }
+                    check_word = check_word >> 1;
+                    alarm_code = alarm_code >> 1;
+                }
+
+            }
+            if (alarm_match > 88)
             {
                 current_direction = (rtd_words[15] >> 2) & 1;
                 line_sync_code = (rtd_words[13] & 0b11111100) | rtd_words[14] >> 6;
@@ -49,6 +60,7 @@ namespace dmsp
             {
                 // Get current channels configuration
                 bool tag_bit = (rtd_frame[1] >> 2) & 1;
+                // bool tag_bit = false;
 
                 // Extract fine pixel data
                 for (int i = 0; i < 15; i++)
