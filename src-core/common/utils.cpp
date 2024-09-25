1a7b3b9658f851addc946b3c384bd340ba6db1f2
Jamie Vital <jvital2013@gmail.com>
2024-09-24 21:20:54 -0400

    Fix building with libcurl < 7.71.0

--- src-core/common/utils.cpp
+++ src-core/common/utils.cpp
@@ -86,7 +86,10 @@ int perform_http_request(std::string url_str, std::string &result)
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_std_string);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
         curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 100);
+
+#ifdef CURLSSLOPT_NATIVE_CA
         curl_easy_setopt(curl, CURLOPT_SSL_OPTIONS, CURLSSLOPT_NATIVE_CA);
+#endif
 
         res = curl_easy_perform(curl);
 
@@ -123,7 +126,10 @@ int perform_http_request_post(std::string url_str, std::string &result, std::str
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, post_req.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_std_string);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+
+#ifdef CURLSSLOPT_NATIVE_CA
         curl_easy_setopt(curl, CURLOPT_SSL_OPTIONS, CURLSSLOPT_NATIVE_CA);
+#endif
 
         res = curl_easy_perform(curl);
 
