7bfed0374931bb0660244ac07329e26fd592005c
Aang23 <qwerty15@gmx.fr>
2024-09-29 19:25:14 +0200

    Split loader from the rest of the plugin

--- plugins/official_products_support/main.cpp
+++ plugins/official_products_support/main.cpp
@@ -13,19 +13,6 @@
 #include "nat2pro/amsu_nat_calibrator.h"
 #include "nat2pro/avhrr_nat_calibrator.h"
 
-//////////
-
-#include "viewer/viewer.h"
-#include "loader/archive_loader.h"
-#include "imgui/imgui_stdlib.h"
-
-namespace
-{
-    bool _enable_loader = false;
-    bool _loader_open = false;
-    std::unique_ptr<satdump::ArchiveLoader> _loader;
-}
-
 class OfficalProductsSupport : public satdump::Plugin
 {
 public:
@@ -38,13 +25,6 @@ public:
     {
         satdump::eventBus->register_handler<RegisterModulesEvent>(registerPluginsHandler);
         satdump::eventBus->register_handler<satdump::ImageProducts::RequestCalibratorEvent>(provideImageCalibratorHandler);
-
-        satdump::eventBus->register_handler<satdump::config::RegisterPluginConfigHandlersEvent>(registerConfigHandler);
-        satdump::eventBus->register_handler<satdump::ViewerApplication::RenderLoadMenuElementsEvent>(renderViewerLoaderButton);
-
-        if (satdump::config::main_cfg.contains("plugin_settings") && satdump::config::main_cfg["plugin_settings"].contains("official_products"))
-            if (satdump::config::main_cfg["plugin_settings"]["official_products"].contains("enable_loader"))
-                _enable_loader = satdump::config::main_cfg["plugin_settings"]["official_products"]["enable_loader"];
     }
 
     static void registerPluginsHandler(const RegisterModulesEvent &evt)
@@ -65,70 +45,6 @@ public:
         //
         else if (evt.id == "mtg_nc_fci")
             evt.calibrators.push_back(std::make_shared<nc2pro::FCINcCalibrator>(evt.calib, evt.products));
-    }
-
-    static void registerConfigHandler(const satdump::config::RegisterPluginConfigHandlersEvent &evt)
-    {
-        evt.plugin_config_handlers.push_back({"Official Products", OfficalProductsSupport::renderConfig, OfficalProductsSupport::saveConfig});
-    }
-
-public:
-    static void initLoader()
-    {
-        if (!_loader)
-        {
-            _loader = std::make_unique<satdump::ArchiveLoader>();
-
-            if (satdump::config::main_cfg.contains("plugin_settings") && satdump::config::main_cfg["plugin_settings"].contains("official_products"))
-            {
-                auto &cfg = satdump::config::main_cfg["plugin_settings"]["official_products"];
-                if (cfg.contains("eumetsat_credentials_key"))
-                    _loader->eumetsat_user_consumer_credential = cfg["eumetsat_credentials_key"];
-                if (cfg.contains("eumetsat_credentials_secret"))
-                    _loader->eumetsat_user_consumer_secret = cfg["eumetsat_credentials_secret"];
-                if (cfg.contains("enable_loader"))
-                    _enable_loader = cfg["enable_loader"];
-            }
-        }
-    }
-
-    static void renderConfig()
-    {
-        initLoader();
-
-        ImGui::Checkbox("Enable Loader", &_enable_loader);
-
-        ImGui::Text("EUMETSAT User Credentials Key");
-        ImGui::SameLine();
-        ImGui::InputText("##eumetsattokenloader_key", &_loader->eumetsat_user_consumer_credential);
-
-        ImGui::Text("EUMETSAT User Credentials Secret");
-        ImGui::SameLine();
-        ImGui::InputText("##eumetsattokenloader_secret", &_loader->eumetsat_user_consumer_secret);
-    }
-
-    static void saveConfig()
-    {
-        satdump::config::main_cfg["plugin_settings"]["official_products"] = {};
-        auto &cfg = satdump::config::main_cfg["plugin_settings"]["official_products"];
-        cfg["eumetsat_credentials_key"] = _loader->eumetsat_user_consumer_credential;
-        cfg["eumetsat_credentials_secret"] = _loader->eumetsat_user_consumer_secret;
-        cfg["enable_loader"] = _enable_loader;
-    }
-
-    static void renderViewerLoaderButton(const satdump::ViewerApplication::RenderLoadMenuElementsEvent &evt)
-    {
-        if (!_enable_loader)
-            return;
-
-        if (ImGui::Button("Load Official"))
-            _loader_open = true;
-
-        if (_loader_open)
-        {
-            initLoader();
-            _loader->drawUI(&_loader_open);
-        }
     }
 };
 
