f4f7eb81627d2b11d3cf6ba2b2ad044f908ab0c1
Marhmarchi <58359447+Marhmarchi@users.noreply.github.com>
2024-10-15 19:44:13 -0300

    help

--- plugins/eos_support/eos/instruments/modis/day_fire.h
+++ plugins/eos_support/eos/instruments/modis/day_fire.h
@@ -2,6 +2,7 @@
 
 #include "common/geodetic/geodetic_coordinates.h"
 #include "common/image/image.h"
+#include "common/image/processing.h"
 #include "common/image/io.h"
 #include "common/projection/projs/equirectangular.h"
 #include "common/projection/sat_proj/sat_proj.h"
@@ -18,22 +19,6 @@
 
 namespace modis
 {
-	//inline float mean(float array[], int n)
-	//{
-	//	float sum = 0;
-	//	for (int i = 0; i < n; i++)
-	//		sum += array[i];
-	//	return sum / n;
-	//}
-
-	//inline float meanabs(float array[], int n)
-	//{
-	//	float abssum = 0;
-	//	for (int i = 0; i < n; i++)
-	//		abssum += abs(array[i] - mean(array, n));
-	//	return abssum / n;
-	//}
-
 	image::Image dayFireCompositor(satdump::ImageProducts *img_pro,
 			std::vector<image::Image> &inputChannels,
 			std::vector<std::string> channelNumbers,
@@ -45,7 +30,7 @@ namespace modis
 	{
 		image::compo_cfg_t f = image::get_compo_cfg(inputChannels, channelNumbers, offsets_cfg);
 
-		image::Image output(f.img_depth, f.maxWidth, f.maxHeight, 4);
+		//image::Image output(f.img_depth, f.maxWidth, f.maxHeight, 4);
 
 		image::Image img_background;
 		image::load_img(img_background, resources::getResourcePath("maps/nasa_hd.jpg"));
@@ -53,180 +38,115 @@ namespace modis
 		equp.init(img_background.width(), img_background.height(), -180, 90, 180, -90);
 		int map_x, map_y;
 
+		image::Image rgb_output(f.img_depth, f.maxWidth, f.maxHeight, 3);
+
 		geodetic::geodetic_coords_t coords;
 		auto proj_cfg = img_pro->get_proj_cfg();
 		auto projFunc = satdump::get_sat_proj(proj_cfg, img_pro->get_tle(), *final_timestamps, true);
 
+		float cfg_offset = vars["minoffset"];
+		float cfg_scalar = vars["scalar"];
+        	float cfg_thresold = vars["thresold"];
+        	bool cfg_blend = vars["blend"];
+        	bool cfg_invert = vars["invert"];
+
 		size_t background_size = img_background.width() * img_background.height();
+		auto &ch_equal = inputChannels[0];
+		image::equalize(ch_equal);
+		float val = 0;
+
+        	float proj_x_scale = 1.0;
+        	if (proj_cfg.contains("width"))
+        	    proj_x_scale = proj_cfg["width"].get<double>() / double(rgb_output.width());
+
+        	float proj_y_scale = 1.0;
+        	if (proj_cfg.contains("height"))
+        	    proj_y_scale = proj_cfg["height"].get<double>() / double(rgb_output.height());
 
 
 
-		//int *channelVals = new int[inputChannels.size()];
-
-		//image::Image tmp(f.img_depth, f.maxWidth, f.maxHeight, 4);
-
-                for (size_t x = 0; x < output.width(); x++)
+                for (size_t x = 0; x < rgb_output.width(); x++)
                 {
-                    for (size_t y = 0; y < output.height(); y++)
+                    for (size_t y = 0; y < rgb_output.height(); y++)
                     {
-			// x - 1, y - 1 + x, y - 1 + x + 1, y - 1
-			// x - 1, y + x, y + x + 1, y
-			// x - 1, y + 1 + x + 1, y + x + 1, y + 1
-			//  ________________________
-			// | -1, -1 | 0, -1 | 1, -1 |
-			// |________________________|
-			// | -1, 0  | 0, 0  | 1, 0  |
-			// |________________________|
-			// | -1, 1  | 1, 0  | 1, 1  |
-			// |________________________|
+			    if (!projFunc->get_position(x * proj_x_scale, y * proj_y_scale, coords))
+			    {
+				    equp.forward(coords.lon, coords.lat, map_x, map_y);
+
+				    if (cfg_invert)
+                    			val = 1.0 - (ch_equal.getf((y * rgb_output.width()) + x) - cfg_offset) * cfg_scalar;
+				    else
+                    			val = (ch_equal.getf((y * rgb_output.width()) + x) - cfg_offset) * cfg_scalar;
+
+				    if (val > 1)
+					val = 1;
+				    if (val < 0)
+					val = 0;
+
+				    int mappos = map_y * img_background.width() + map_x;
+
+				    if (mappos >= (int)background_size)
+					mappos = background_size - 1;
+
+				    if (mappos < 0)
+					mappos = 0;
 
 
-			//float sum_final = 0.0;
-			//float sumt4 = 0.0;
-			//float sumt11 = 0.0;
-			//float meant4 = 0.0;
-			//float meant11 = 0.0;
-			//float mean_final = 0.0;
-			//float stddev = 0.0;
-
-			//float t4_22_array[9] = { 0 };
-			//float t11_array[9] = { 0 };
-
-			//float deltaT[9] = { 0 };
-
-			//t4_22_array[0] += img_pro->get_calibrated_value(24, x - 1, y - 1, true);
-			//t4_22_array[1] += img_pro->get_calibrated_value(24, x, y - 1, true);
-			//t4_22_array[2] += img_pro->get_calibrated_value(24, x + 1, y - 1, true);
-			//t4_22_array[3] += img_pro->get_calibrated_value(24, x - 1, y, true);
-			//t4_22_array[4] += img_pro->get_calibrated_value(24, x, y, true);
-			//t4_22_array[5] += img_pro->get_calibrated_value(24, x + 1, y, true);
-			//t4_22_array[6] += img_pro->get_calibrated_value(24, x - 1, y + 1, true);
-			//t4_22_array[7] += img_pro->get_calibrated_value(24, x, y + 1, true);
-			//t4_22_array[8] += img_pro->get_calibrated_value(24, x + 1, y + 1, true);
-
-			//t11_array[0] += img_pro->get_calibrated_value(33, x - 1, y - 1, true);
-			//t11_array[1] += img_pro->get_calibrated_value(33, x, y - 1, true);
-			//t11_array[2] += img_pro->get_calibrated_value(33, x + 1, y - 1, true);
-			//t11_array[3] += img_pro->get_calibrated_value(33, x - 1, y, true);
-			//t11_array[4] += img_pro->get_calibrated_value(33, x, y, true);
-			//t11_array[5] += img_pro->get_calibrated_value(33, x + 1, y, true);
-			//t11_array[6] += img_pro->get_calibrated_value(33, x - 1, y + 1, true);
-			//t11_array[7] += img_pro->get_calibrated_value(33, x, y + 1, true);
-			//t11_array[8] += img_pro->get_calibrated_value(33, x + 1, y + 1, true);
-
-
-
-			//for (int i = 0; i < 9; ++i)
-			//	sumt4 += t4_22_array[i];
-
-			//meant4 = ((float)sumt4) / 9;
-
-
-			//for (int i = 0; i < 9; ++i)
-			//	sumt11 += t11_array[i];
-
-			//meant11 = ((float)sumt11) / 9;
-
-			//mean_final = meant4 - meant11 / 2;
-
-			//printf("Delta T : %f", mean_final);
+				    for (int c = 0; c < 3; c++)
+				    {
+					    float fval = img_background.getf(background_size * c + mappos);
+					    rgb_output.setf(c, y * rgb_output.width() + x, rgb_output.clampf(fval));
+				    }
 
 
 
 
-			//for (int i = 0; i < 9; ++i)
-			//	deltaT[i] += t4_22_array[i] + t11_array[i];
-
-			//for (int i = 0; i < 9; ++i)
-			//	sum += deltaT[i];
-
-			//mean = ((float)sum) / 9;
-
-			//printf("Mean : %f", sum);
+                                    //int t4_21 = img_pro->get_calibrated_value(23, x, y, true);
+                                    //int t4_22 = img_pro->get_calibrated_value(24, x, y, true);
+                                    //int t11 = img_pro->get_calibrated_value(33, x, y, true);
+                                    //int t12 = img_pro->get_calibrated_value(34, x, y, true);
 
 
 
-			//for (int i = 0; i < 9; ++i)
-			//	sum += t4_22_array[i];
 
-			//mean = ((float)sum) / 9;
+			            //if (t12 < 0)
+			            //{
+			            //	t12 = 0;
+			            //}
 
+			            //if (t4_22 < 0)
+			            //{
+			            //	t4_22 = 0;
+			            //}
+			            //if (t4_21 < 0)
+			            //{
+			            //	t4_21 = 0;
+			            //}
+			            //if (t11 < 0)
+			            //{
+			            //	t11 = 0;
+			            //}
 
-			//for (int i = 0; i < 9; ++i)
-			//	stddev += sqrt(pow(t4_22_array[i] - mean, 2) / 9);
-
-			//printf("Mean : %f Standard deviation : %f\n", mean, stddev);
-			//printf("Mean : %f Standard deviation : %f\n", mean, meanabs(t4_22_array, 9));
-
-
-
-                        int t4_21 = img_pro->get_calibrated_value(23, x, y, true);
-                        int t4_22 = img_pro->get_calibrated_value(24, x, y, true);
-                        int t11 = img_pro->get_calibrated_value(33, x, y, true);
-                        int t12 = img_pro->get_calibrated_value(34, x, y, true);
-
-			if (t12 < 0)
-			{
-				t12 = 0;
-			}
-
-			if (t4_22 < 0)
-			{
-				t4_22 = 0;
-			}
-			if (t4_21 < 0)
-			{
-				t4_21 = 0;
-			}
-			if (t11 < 0)
-			{
-				t11 = 0;
-			}
-
-			if (t12 >= 265 && t4_22 > 310 && t4_22 - t11 > 15)
-			{
-				output.set(0, y * output.width() + x, 1);
-				output.set(1, y * output.width() + x, 0);
-				output.set(2, y * output.width() + x, 0);
-				output.set(3, y * output.width() + x, 1);
-			}
-
-			//if (t4_22 > 310 && t4_22 - t11 > 25)
-			//{
-			//	//printf("T4_21 : %d", t4_21);
-			//	output.set(0, y * output.width() + x, 1);
-			//	output.set(1, y * output.width() + x, 0);
-			//	output.set(2, y * output.width() + x, 0);
-			//	output.set(3, y * output.width() + x, 1);
-			//}
-			else
-			{
-				output.set(0, y * output.width() + x, 0);
-				output.set(1, y * output.width() + x, 0);
-				output.set(2, y * output.width() + x, 0);
-				output.set(3, y * output.width() + x, 0);
-			}
-
-			//int t4_21 = channelVals[0];
-			//int t4_22 = channelVals[1];
-			//int t11 = channelVals[2];
-
-			//printf("T4_21 : %d T4_22 : %d T11 : %d\n", t4_21, t4_22, t11);
-			//logger->info("T4_21 : %d", t4_21);
-
-                        //output.set(0, y * output.width() + x, t4_21);
-                        //output.set(1, y * output.width() + x, t4_22);
-                        //output.set(2, y * output.width() + x, t11);
-
-                        // return RGB 0=R 1=G 2=B
-                        //output.set(0, y * output.width() + x, channelVals[0]);
-                        //output.set(1, y * output.width() + x, channelVals[1]);
-                        //output.set(2, y * output.width() + x, channelVals[2]);
+			            //if (t12 >= 265 && t4_22 > 310 && t4_22 - t11 > 15)
+			            //{
+			            //	output.set(0, y * output.width() + x, 1);
+			            //	output.set(1, y * output.width() + x, 0);
+			            //	output.set(2, y * output.width() + x, 0);
+			            //	output.set(3, y * output.width() + x, 1);
+			            //}
+			            //else
+			            //{
+			            //	output.set(0, y * output.width() + x, 0);
+			            //	output.set(1, y * output.width() + x, 0);
+			            //	output.set(2, y * output.width() + x, 0);
+			            //	output.set(3, y * output.width() + x, 0);
+			            //}
+			    }
                     }
 
                     // set the progress bar accordingly
                     if (progress != nullptr)
-                        *progress = double(x) / double(output.width());
+                        *progress = double(x) / double(rgb_output.width());
                 }
 
 	//delete[] channelVals;
@@ -238,6 +158,6 @@ namespace modis
         //hueTunning.overlap = 100.0 / 100.0;
         //image::hue_saturation(output, hueTunning);
 
-        return output;
+        return rgb_output;
     }
 }
