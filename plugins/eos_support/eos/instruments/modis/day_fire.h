f947feeff2e7b155e0bae1d660c8267d3e096f6c
Julia <58359447+juliasatt@users.noreply.github.com>
2024-10-19 16:33:31 -0300

    Update day_fire.h

--- plugins/eos_support/eos/instruments/modis/day_fire.h
+++ plugins/eos_support/eos/instruments/modis/day_fire.h
@@ -18,82 +18,82 @@
 
 namespace modis
 {
-	image::Image dayFireCompositor(satdump::ImageProducts *img_pro,
-			std::vector<image::Image> &inputChannels,
-			std::vector<std::string> channelNumbers,
-			std::string /* cpp_id */,
-			nlohmann::json vars,
-			nlohmann::json offsets_cfg,
-			std::vector<double> *final_timestamps = nullptr,
-			float *progress = nullptr)
+    image::Image dayFireCompositor(satdump::ImageProducts *img_pro,
+			               std::vector<image::Image> &inputChannels,
+			               std::vector<std::string> channelNumbers,
+			               std::string /* cpp_id */,
+			               nlohmann::json vars,
+			               nlohmann::json offsets_cfg,
+			               std::vector<double> *final_timestamps = nullptr,
+			               float *progress = nullptr)
+    {
+	image::compo_cfg_t f = image::get_compo_cfg(inputChannels, channelNumbers, offsets_cfg);
+
+	image::Image img_background;
+	image::load_img(img_background, resources::getResourcePath("maps/nasa_hd.jpg"));
+	geodetic::projection::EquirectangularProjection equp;
+	equp.init(img_background.width(), img_background.height(), -180, 90, 180, -90);
+	int map_x, map_y;
+
+	image::Image rgb_output(f.img_depth, f.maxWidth, f.maxHeight, 4);
+
+	geodetic::geodetic_coords_t coords;
+	auto proj_cfg = img_pro->get_proj_cfg();
+	auto projFunc = satdump::get_sat_proj(proj_cfg, img_pro->get_tle(), *final_timestamps, true);
+
+	float cfg_offset = vars["minoffset"];
+	float cfg_scalar = vars["scalar"];
+	bool map_overlay = vars["map_overlay"];
+
+	size_t background_size = img_background.width() * img_background.height();
+	auto &ch_equal = inputChannels[0];
+	image::equalize(ch_equal);
+	float val = 0;
+
+	float proj_scale = 1.0;
+	if (proj_cfg.contains("image_width"))
+	    proj_scale = proj_cfg["image_width"].get<double>() / double(rgb_output.width());
+
+
+	if (map_overlay == true)
 	{
-		image::compo_cfg_t f = image::get_compo_cfg(inputChannels, channelNumbers, offsets_cfg);
-
-		image::Image img_background;
-		image::load_img(img_background, resources::getResourcePath("maps/nasa_hd.jpg"));
-		geodetic::projection::EquirectangularProjection equp;
-		equp.init(img_background.width(), img_background.height(), -180, 90, 180, -90);
-		int map_x, map_y;
-
-		image::Image rgb_output(f.img_depth, f.maxWidth, f.maxHeight, 4);
-
-		geodetic::geodetic_coords_t coords;
-		auto proj_cfg = img_pro->get_proj_cfg();
-		auto projFunc = satdump::get_sat_proj(proj_cfg, img_pro->get_tle(), *final_timestamps, true);
-
-		float cfg_offset = vars["minoffset"];
-		float cfg_scalar = vars["scalar"];
-		bool map_overlay = vars["map_overlay"];
-
-		size_t background_size = img_background.width() * img_background.height();
-		auto &ch_equal = inputChannels[0];
-		image::equalize(ch_equal);
-		float val = 0;
-
-		float proj_scale = 1.0;
-		if (proj_cfg.contains("image_width"))
-			proj_scale = proj_cfg["image_width"].get<double>() / double(rgb_output.width());
-
-
-		if (map_overlay == true)
-		{
-                for (size_t x = 0; x < rgb_output.width(); x++)
+            for (size_t x = 0; x < rgb_output.width(); x++)
+	    {
+                for (size_t y = 0; y < rgb_output.height(); y++)
                 {
-                    for (size_t y = 0; y < rgb_output.height(); y++)
-                    {
-			    if (!projFunc->get_position(x * proj_scale, y * proj_scale, coords))
-			    {
-				    equp.forward(coords.lon, coords.lat, map_x, map_y);
+		    if (!projFunc->get_position(x * proj_scale, y * proj_scale, coords))
+		    {
+			equp.forward(coords.lon, coords.lat, map_x, map_y);
 
-				    val = (ch_equal.getf((y * rgb_output.width()) + x) - cfg_offset) * cfg_scalar;
+			val = (ch_equal.getf((y * rgb_output.width()) + x) - cfg_offset) * cfg_scalar;
 
-				    if (val > 1)
-					val = 1;
-				    if (val < 0)
-					val = 0;
+			if (val > 1)
+			    val = 1;
+			if (val < 0)
+			    val = 0;
 
-				    int mappos = map_y * img_background.width() + map_x;
+			int mappos = map_y * img_background.width() + map_x;
 
-				    if (mappos >= (int)background_size)
-					mappos = background_size - 1;
+			if (mappos >= (int)background_size)
+			    mappos = background_size - 1;
 
-				    if (mappos < 0)
-					mappos = 0;
+			if (mappos < 0)
+			    mappos = 0;
 
-				    for (int c = 0; c < 3; c++)
-				    {
-					    float mval = img_background.getf(background_size * c + mappos);
-					    rgb_output.setf(c, y * rgb_output.width() + x, rgb_output.clampf(mval));
-				    }
-				    rgb_output.setf(3, y * rgb_output.width() + x, rgb_output.clampf(1));
+			for (int c = 0; c < 3; c++)
+			{
+			    float mval = img_background.getf(background_size * c + mappos);
+			    rgb_output.setf(c, y * rgb_output.width() + x, rgb_output.clampf(mval));
+			}
+			    rgb_output.setf(3, y * rgb_output.width() + x, rgb_output.clampf(1));
 
-			    }
-		    }
-		    // set first progerss bar
-		    if (progress != nullptr)
-                        *progress = double(x) / double(rgb_output.width());
+			}
 		}
-		}
+		// set first progerss bar
+		if (progress != nullptr)
+                    *progress = double(x) / double(rgb_output.width());
+	    }
+	}
 
 		//for (size_t x = 0; x < rgb_output.width(); x++)
 		//{
@@ -121,47 +121,46 @@ namespace modis
 		//	}
 		//}
 
-                for (size_t x = 0; x < rgb_output.width(); x++)
+	for (size_t x = 0; x < rgb_output.width(); x++)
+            {
+		for (size_t y = 0; y < rgb_output.height(); y++)
                 {
-                    for (size_t y = 0; y < rgb_output.height(); y++)
-                    {
+		    int t4_21 = img_pro->get_calibrated_value(23, x, y, true);
+		    int t4_22 = img_pro->get_calibrated_value(24, x, y, true);
+                    int t11 = img_pro->get_calibrated_value(33, x, y, true);
+                    int t12 = img_pro->get_calibrated_value(34, x, y, true);
 
-			    int t4_21 = img_pro->get_calibrated_value(23, x, y, true);
-			    int t4_22 = img_pro->get_calibrated_value(24, x, y, true);
-                            int t11 = img_pro->get_calibrated_value(33, x, y, true);
-                            int t12 = img_pro->get_calibrated_value(34, x, y, true);
-
-			    if (t12 < 0)
-                            {
-                            	t12 = 0;
-                            }
+		if (t12 < 0)
+                {
+		    t12 = 0;
+                }
                             
-                            if (t4_22 < 0)
-                            {
-                            	t4_22 = 0;
-                            }
-                            if (t4_21 < 0)
-                            {
-                            	t4_21 = 0;
-                            }
-                            if (t11 < 0)
-                            {
-                            	t11 = 0;
-                            }
-
-			    //if (t12 >= 265 && t4_22 > 310 && t4_22 - t11 > 15)
-                            
-                            if (t12 >= 265 && t4_22 > 310 && t4_22 - t11 > 15)
-                            {
-                                    //rgb_output.draw_circle(x, y, 3, {1.0,0,0}, true);
-				    rgb_output.draw_rectangle(x - 2, y + 2, x + 2, y - 2, {1,0,0,1}, true);
-                            }
-                    }
-                    // set second progress bar
-                    if (progress != nullptr)
-                        *progress = double(x) / double(rgb_output.width());
+                if (t4_22 < 0)
+                {
+                    t4_22 = 0;
+                }
+                if (t4_21 < 0)
+                {
+                    t4_21 = 0;
+                }
+                if (t11 < 0)
+                {
+                    t11 = 0;
                 }
 
-        return rgb_output;
+		//if (t12 >= 265 && t4_22 > 310 && t4_22 - t11 > 15)
+                            
+                if (t12 >= 265 && t4_22 > 310 && t4_22 - t11 > 15)
+                {
+		    //rgb_output.draw_circle(x, y, 3, {1.0,0,0}, true);
+		    rgb_output.draw_rectangle(x - 2, y + 2, x + 2, y - 2, {1,0,0,1}, true);
+		}
+		}
+		// set second progress bar
+		if (progress != nullptr)
+                    *progress = double(x) / double(rgb_output.width());
+	    }
+	    
+	return rgb_output;
     }
 }
