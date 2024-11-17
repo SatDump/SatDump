a9afe3d7a4376aae3f5c86e85dff2ab6bb01e5fa
Jamie Vital <jvital2013@gmail.com>
2024-11-17 09:42:34 -0500

    Update Space Track API

--- src-core/common/utils.h
+++ src-core/common/utils.h
@@ -130,8 +130,12 @@ bool isStringPresent(std::string searched, std::string keyword);
 // Return filesize
 uint64_t getFilesize(std::string filepath);
 
+// cURL helper function
+size_t curl_write_std_string(void* contents, size_t size, size_t nmemb, std::string* s);
+
 // Perform a HTTP Request on the provided URL and return the result as a string
 int perform_http_request(std::string url, std::string &result, std::string added_header = "", float *progress = nullptr);
+
 // Perform a HTTP Request on the provided URL and return the result as a string, with POST data
 int perform_http_request_post(std::string url_str, std::string &result, std::string post_req, std::string added_header = "");
 
