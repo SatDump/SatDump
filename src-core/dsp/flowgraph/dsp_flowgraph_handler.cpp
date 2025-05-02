55cb635ab248ba250b0a2cce3b6caf8b4a6a54a0
juliasat <58359447+juliasatt@users.noreply.github.com>
2025-05-02 16:37:34 -0300

    add cyclo block to flowgraph

--- src-core/dsp/flowgraph/dsp_flowgraph_handler.cpp
+++ src-core/dsp/flowgraph/dsp_flowgraph_handler.cpp
@@ -2,6 +2,7 @@
 #include "core/plugin.h"
 #include "dsp/device/dev.h"
 #include "dsp/filter/rrc.h"
+#include "dsp/utils/cyclostationary_analysis.h"
 #include "imgui/imnodes/imnodes.h"
 #include "logger.h"
 
@@ -17,6 +18,8 @@
 #include "dsp/clock_recovery/clock_recovery_mm.h"
 #include "dsp/const/const_disp.h"
 #include "dsp/pll/costas.h"
+
+#include <memory>
 
 #include "nlohmann/json_utils.h"
 
@@ -99,6 +102,10 @@ namespace satdump
 
             flowgraph.node_internal_registry.insert(
                 {"rrc_fir_cc", {"RRC FIR CC", [](const ndsp::Flowgraph *f) { return std::make_shared<ndsp::NodeInternal>(f, std::make_shared<ndsp::RRC_FIRBlock<complex_t>>()); }}});
+
+            flowgraph.node_internal_registry.insert(
+                {"cyclostationary_analysis_cf",
+                 {"Cyclostationary/Cyclostationary Analysis", [](const ndsp::Flowgraph *f) { return std::make_shared<ndsp::NodeInternal>(f, std::make_shared<ndsp::CyclostationaryAnalysis>()); }}});
 
             //   flowgraph.node_internal_registry.insert({"airspy_dev_cc", {"Airspy Dev", [=](const ndsp::Flowgraph *f)
             //                                                              { return std::make_shared<ndsp::NodeInternal>(f, std::make_shared<ndsp::AirspyDevBlock>()); }}});
