2f2031b079d70e38cc8b78b88d287dd4e635cdf1
Julia <58359447+juliasatt@users.noreply.github.com>
2025-05-26 11:30:35 -0300

    Update hist_disp.h

--- src-core/dsp/displays/hist_disp.h
+++ src-core/dsp/displays/hist_disp.h
@@ -21,17 +21,11 @@ namespace satdump
 
             nlohmann::json get_cfg(std::string key)
             {
-                // if (key == "max_gain")
-                //     return p_max_gain;
-                // else
                 throw satdump_exception(key);
             }
 
             cfg_res_t set_cfg(std::string key, nlohmann::json v)
             {
-                // if (key == "max_gain")
-                //     p_max_gain = v;
-                // else
                 throw satdump_exception(key);
                 return RES_OK;
             }
