f66eb87300a847c8a1959361be5d9abcd45c5359
Aweeri <aweeri.eu@gmail.com>
2025-07-05 19:25:49 +0200

    added a config editor plugin

---
+++ plugins/wip_settings_editor_app/main.cpp
@@ -0,0 +1,34 @@
+#include "core/plugin.h"
+#include "logger.h"
+
+#include "app.h"
+#include "explorer/explorer.h"
+#include "settings_editor.h"
+
+class WipSettingsEditorAppPlugin : public satdump::Plugin
+{
+public:
+    std::string getID() { return "wipsettings_app"; }
+
+    void init()
+    {
+        satdump::eventBus->register_handler<satdump::explorer::ExplorerApplication::RenderLoadMenuElementsEvent>(renderExplorerLoaderButton);
+    }
+
+    static void renderExplorerLoaderButton(const satdump::explorer::ExplorerApplication::RenderLoadMenuElementsEvent &evt)
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            if (ImGui::BeginMenu("Tools"))
+            {
+                if (ImGui::MenuItem("Instrument Config Generator"))
+                    evt.master_handler->addSubHandler(std::make_shared<satdump::WipSettingsEditorHandler>());
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMenu();
+        }
+    }
+};
+
+PLUGIN_LOADER(WipSettingsEditorAppPlugin)
