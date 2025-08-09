963f7eb4d54839e3b1dbaa95a232d7e7dda344f3
Aang23 <qwerty15@gmx.fr>
2025-08-09 14:56:51 +0200

    Fix lego's grass blade bugs

--- plugins/sdr_sources/bladerf_sdr_support/bladerf_dev.h
+++ plugins/sdr_sources/bladerf_sdr_support/bladerf_dev.h
@@ -4,6 +4,7 @@
 #include "dsp/block.h"
 #include "dsp/device/dev.h"
 
+#include <cstdint>
 #include <cstdio>
 #include <libbladeRF.h>
 #include <string>
@@ -46,8 +47,11 @@ namespace satdump
 
             bool extclock = false;
 
+            // TODOREWORK, not here but in the config add back bandwidth/manual banwidth?
+
         private:
             int sample_buffer_size = 8192; // TODOREWORK
+            bool is_8bit = false;
 
             std::thread work_thread_rx;
             bool thread_should_run_rx = false;
@@ -67,7 +71,10 @@ namespace satdump
                         if (rx_ch_number == 1)
                         {
                             DSPBuffer oblk = DSPBuffer::newBufferSamples<complex_t>(sample_buffer_size);
-                            volk_16i_s32f_convert_32f((float *)oblk.getSamples<complex_t>(), sample_buffer, 4096.0f, sample_buffer_size * 2);
+                            if (is_8bit)
+                                volk_8i_s32f_convert_32f((float *)oblk.getSamples<complex_t>(), (int8_t *)sample_buffer, 127.0f, sample_buffer_size * 2);
+                            else
+                                volk_16i_s32f_convert_32f((float *)oblk.getSamples<complex_t>(), sample_buffer, 4096.0f, sample_buffer_size * 2);
                             oblk.size = meta.actual_count;
                             outputs[0].fifo->wait_enqueue(oblk);
                         }
@@ -78,10 +85,21 @@ namespace satdump
                             {
                                 DSPBuffer oblk = DSPBuffer::newBufferSamples<complex_t>(sample_buffer_size);
                                 auto c = oblk.getSamples<complex_t>();
-                                for (int i = 0; i < nsam; i++)
+                                if (is_8bit)
                                 {
-                                    c[i].real = sample_buffer[(i * 2 + s) * 2 + 0] / 4096.0f;
-                                    c[i].imag = sample_buffer[(i * 2 + s) * 2 + 1] / 4096.0f;
+                                    for (int i = 0; i < nsam; i++)
+                                    {
+                                        c[i].real = ((int8_t *)sample_buffer)[(i * 2 + s) * 2 + 0] / 127.0f;
+                                        c[i].imag = ((int8_t *)sample_buffer)[(i * 2 + s) * 2 + 1] / 127.0f;
+                                    }
+                                }
+                                else
+                                {
+                                    for (int i = 0; i < nsam; i++)
+                                    {
+                                        c[i].real = sample_buffer[(i * 2 + s) * 2 + 0] / 4096.0f;
+                                        c[i].imag = sample_buffer[(i * 2 + s) * 2 + 1] / 4096.0f;
+                                    }
                                 }
                                 oblk.size = nsam;
                                 outputs[s].fifo->wait_enqueue(oblk);
@@ -117,7 +135,10 @@ namespace satdump
                                 break;
                             }
 
-                            volk_32f_s32f_convert_16i(sample_buffer, (float *)iblk.getSamples<complex_t>(), 4096.0f, iblk.size * 2);
+                            if (is_8bit)
+                                volk_32f_s32f_convert_8i((int8_t *)sample_buffer, (float *)iblk.getSamples<complex_t>(), 127.0f, iblk.size * 2);
+                            else
+                                volk_32f_s32f_convert_16i(sample_buffer, (float *)iblk.getSamples<complex_t>(), 4096.0f, iblk.size * 2);
                             if (bladerf_sync_tx(bladerf_dev_obj, sample_buffer, iblk.size, &meta, 4000) != 0)
                                 std::this_thread::sleep_for(std::chrono::seconds(1));
 
