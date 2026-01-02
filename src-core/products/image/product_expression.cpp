8e429a4f35a2b6cd8d8142054f2e72459a798cac
Cpt-Dingus <100243410+Cpt-Dingus@users.noreply.github.com>
2026-01-02 22:09:47 +0100

    Expression now aborts if LUT doesn't exist

--- src-core/products/image/product_expression.cpp
+++ src-core/products/image/product_expression.cpp
@@ -385,6 +385,13 @@ namespace satdump
                 for (auto &l : lut_cfgs)
                 {
                     logger->trace("Needs LUT " + l.path + " as " + l.token);
+
+                    if (!std::filesystem::exists(resources::getResourcePath(l.path)))
+                    {
+                        // LUT does not exist, abort!
+                        throw satdump_exception("A specified LUT does not exist");
+                    }
+
                     std::shared_ptr<image::Image> lutimg = std::make_shared<image::Image>();
                     image::load_img(*lutimg, resources::getResourcePath(l.path));
                     equParser.DefineFunUserData(l.token.c_str(), lutProcess, lutimg.get());
