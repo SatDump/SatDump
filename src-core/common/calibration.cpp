ab0ddc27c2065a54e7039ae6d5fc3582bf76a309
Adrian Bunk <bunk@debian.org>
2026-02-14 12:39:12 +0200

    Fix the build with volk 3.3

--- src-core/common/calibration.cpp
+++ src-core/common/calibration.cpp
@@ -3,9 +3,9 @@
 #include <vector>
 #include <ctime>
 
-double temperature_to_radiance(double t, double v) { return (c1 * v * v * v) / (pow(e_num, c2 * v / t) - 1); }
+double temperature_to_radiance(double t, double v) { return (1.1910427e-05 * v * v * v) / (pow(e_num, 1.4387752 * v / t) - 1); }
 
-double radiance_to_temperature(double L, double v) { return (c2 * v) / (log(c1 * v * v * v / L + 1)); }
+double radiance_to_temperature(double L, double v) { return (1.4387752 * v) / (log(1.1910427e-05 * v * v * v / L + 1)); }
 
 double freq_to_wavenumber(double freq) { return 1e7 / ((299792458.0 / freq) / 10e-10); }
 
--- src-core/common/calibration.h
+++ src-core/common/calibration.h
@@ -4,8 +4,6 @@
 #include <map>
 #include <ctime>
 
-#define c1 1.1910427e-05
-#define c2 1.4387752
 #define e_num 2.7182818
 
 double temperature_to_radiance(double t, double v);
