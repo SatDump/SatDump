c337b9bfcc9e627be9851ddafa3a6689f9602244
Cpt-Dingus <100243410+Cpt-Dingus@users.noreply.github.com>
2026-02-19 18:38:56 +0100

    Bundles the program the right way now

--- macOS/bundle.sh
+++ macOS/bundle.sh
@@ -60,94 +60,47 @@ then
     SIGN_FLAG="-ns"
 fi
 
-echo "Copying libraries..."
-mkdir MacApp/SatDump.app/Contents/libs
-cp $GITHUB_WORKSPACE/deps/lib/*.dylib MacApp/SatDump.app/Contents/libs
-# We are already in the build dir, that's where the satdump dylibs are - ./ and ./plugins
-cp ./*.dylib MacApp/SatDump.app/Contents/libs
-cp ./plugins/*.dylib MacApp/SatDump.app/Contents/libs # TODOREWORK figure out why satdump craps the bed without this line
-
-# Symlinks are not copied by dylibbuilder, we gotta copy dependencies manually from homebrew.
-# This enables running without homebrew deps installed system-wide
-# Surely there has to be a better way to do this? This should work for the time being,
-# as these paths should be standardized. Extremely hands-on approach though.
-cp $HOMEBREW_LIB/lib/libjemalloc*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libcpu_features*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/liborc*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libarpack*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libglfw*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libarmadillo*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libvolk*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libpng*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libfftw*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libnng*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libzstd*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libtiff*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libusb*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libportaudio*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libhdf5*.dylib MacApp/SatDump.app/Contents/libs
-
-# Dependencies for libs (not satdump directly)
-cp $HOMEBREW_LIB/lib/libmpi*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libsz*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libaec*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libopen-pal*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libevent*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libssl*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libcrypto*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libhwloc*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libpmix*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/liblzma*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libjpeg*.dylib MacApp/SatDump.app/Contents/libs
-
-# boost has a crap ton of modules, selected individually so we don't copy unnecessary stuff
-cp $HOMEBREW_LIB/lib/libboost_atomic*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libboost_container*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libboost_chrono*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libboost_date*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libboost_filesystem*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libboost_program*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libboost_serialization*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libboost_thread*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/lib/libboost_unit*.dylib MacApp/SatDump.app/Contents/libs
-
-
-# These are not in /lib becuase they were born that way, we gotta use the full path
-cp $HOMEBREW_LIB/opt/libomp/lib/libomp*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/opt/openblas/lib/libopenblas.0*.dylib MacApp/SatDump.app/Contents/libs
-cp $HOMEBREW_LIB/opt/gfortran/lib/gcc/current/*.dylib MacApp/SatDump.app/Contents/libs
-
-
-echo "Patching absolute dependency paths..."
-
-# We copy libraries but their dependencies still point to the homebrew path, we gotta replace with an rpath
-for lib in MacApp/SatDump.app/Contents/libs/*.dylib; do
-    # dylib's own ID
-    install_name_tool -id @rpath/$(basename "$lib") "$lib"
-
-    # lib's dependencies, pipefail is on so we must '|| true' as not every dependecy has
-    # dependencies in the homebrew paths
-    deps=$(otool -L "$lib" | awk '{print $1}' | grep "^$HOMEBREW_LIB/") || true
-
-    if [[ -n "$deps" ]]; then
-        while read -r dep; do
-            install_name_tool -change "$dep" "@rpath/$(basename "$dep")" "$lib"
-        done <<< "$deps"
-    fi
-done
-
-echo "Re-linking binaries"
+# Omp, openblas, and gfortran are not in $HOMEBREW_LIB/lib for some ungodly reason; we include their full paths instead
+echo "Packaging and re-linking libraries..."
 plugin_args=$(ls MacApp/SatDump.app/Contents/Resources/plugins | xargs printf -- '-x MacApp/SatDump.app/Contents/Resources/plugins/%s ')
 dylibbundler $SIGN_FLAG \
   -cd \
-  -s $HOMEBREW_LIB/lib \
-  -s $GITHUB_WORKSPACE/deps/lib \
+  -b \
+  -of \
   -s . \
+  -s $GITHUB_WORKSPACE/deps/lib \
+  -s $HOMEBREW_LIB/lib \
+  -s $HOMEBREW_LIB/opt/libomp/lib \
+  -s $HOMEBREW_LIB/opt/openblas/lib \
+  -s $HOMEBREW_LIB/opt/gfortran/lib/gcc/current \
   -d MacApp/SatDump.app/Contents/libs \
   -x MacApp/SatDump.app/Contents/MacOS/satdump-ui \
   -x MacApp/SatDump.app/Contents/MacOS/satdump_sdr_server \
   -x MacApp/SatDump.app/Contents/MacOS/satdump \
   $plugin_args
+
+# SDRPlay is custom, not staticaly linked; we can copy it manually
+cp $GITHUB_WORKSPACE/deps/lib/libsdrplay*.dylib MacApp/SatDump.app/Contents/libs
+
+
+# Some libraries are processed more than once, dylibbundler is silly and doesn't check whether
+# it injects multiple LC_RPATH entries. MacOS is pissy about it and refuses to work with more than one,
+# so we have to remove the duplicates manually.
+echo "Removing duplicate RPATH entries..."
+find MacApp/SatDump.app/Contents/libs -name "*.dylib" | while read lib; do
+    rpaths=($(otool -l "$lib" | awk '/LC_RPATH/{getline; getline; sub(/.*path /,""); sub(/ .*/,""); print}'))
+    seen=()
+    for rp in "${rpaths[@]}"; do
+        if [[ " ${seen[*]} " != *" $rp "* ]]; then
+            seen+=("$rp")
+        else
+            install_name_tool -delete_rpath "$rp" "$lib"
+        fi
+    done
+
+    # We have to resign the libraries afterwards as we changed their Mach-O headers
+    codesign -v --force --timestamp --sign - "$lib"
+done
 
 
 if [[ -n "$MACOS_SIGNING_SIGNATURE" ]]
@@ -166,26 +119,6 @@ then
     codesign -v --force --options runtime --entitlements $GITHUB_WORKSPACE/macOS/Entitlements.plist --timestamp --sign "$MACOS_SIGNING_SIGNATURE" MacApp/SatDump.app/Contents/MacOS/satdump
     codesign -v --force --options runtime --entitlements $GITHUB_WORKSPACE/macOS/Entitlements.plist --timestamp --sign "$MACOS_SIGNING_SIGNATURE" MacApp/SatDump.app/Contents/MacOS/satdump_sdr_server
     codesign -v --force --options runtime --entitlements $GITHUB_WORKSPACE/macOS/Entitlements.plist --timestamp --sign "$MACOS_SIGNING_SIGNATURE" MacApp/SatDump.app/Contents/MacOS/satdump-ui
-
-else 
-    echo "No signature found, signing with ad-hoc signature..."
-    # Since we adjusted homebrew paths in dylibs, we must resign the libs
-    # We don't have a proper signature, so an ad-hoc one will have to do...
-    for lib in MacApp/SatDump.app/Contents/libs/*.dylib; do
-        codesign -v --force --timestamp --sign - $lib
-    done
-
-    for dylib in MacApp/SatDump.app/Contents/Resources/plugins/*.dylib
-    do
-	    codesign -v --force --timestamp --sign - $dylib
-    done
-
-    codesign -v --force --options runtime --entitlements $GITHUB_WORKSPACE/macOS/Entitlements.plist --timestamp --sign - MacApp/SatDump.app/Contents/MacOS/satdump
-    codesign -v --force --options runtime --entitlements $GITHUB_WORKSPACE/macOS/Entitlements.plist --timestamp --sign - MacApp/SatDump.app/Contents/MacOS/satdump_sdr_server
-    codesign -v --force --options runtime --entitlements $GITHUB_WORKSPACE/macOS/Entitlements.plist --timestamp --sign - MacApp/SatDump.app/Contents/MacOS/satdump-ui
-
-    # Finally, sign the bundle
-    codesign --force --deep --sign - MacApp/SatDump.app
 fi
 
 echo "Creating SatDump.dmg..."
