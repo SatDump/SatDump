315b056b6aee36f02fa9f92f511e6ebca7fd508e
Aang23 <qwerty15@gmx.fr>
2026-05-05 21:56:25 +0200

    Oops

---
+++ src-core/dsp/filter/decimating_fir.cpp
@@ -0,0 +1,154 @@
+#include "decimating_fir.h"
+#include "dsp/block_helpers.h"
+
+namespace satdump
+{
+    namespace ndsp
+    {
+        template <typename T, typename TT>
+        DecimatingFIRBlock<T, TT>::DecimatingFIRBlock(std::string id)
+            : BlockSimple<T, T>(id + "decimating_fir" + (std::is_same_v<TT, complex_t> ? "c" : "") + "_" + getShortTypeName<T>() + getShortTypeName<T>(), {{"in", getTypeSampleType<T>()}},
+                                {{"out", getTypeSampleType<T>()}})
+        {
+        }
+
+        template <typename T, typename TT>
+        DecimatingFIRBlock<T, TT>::~DecimatingFIRBlock()
+        {
+            if (taps != nullptr)
+            {
+                for (int i = 0; i < aligned_tap_count; i++)
+                    volk_free(taps[i]);
+                volk_free(taps);
+            }
+        }
+
+        template <typename T, typename TT>
+        void DecimatingFIRBlock<T, TT>::init()
+        {
+            // Free if needed
+            if (taps != nullptr)
+            {
+                for (int i = 0; i < aligned_tap_count; i++)
+                    volk_free(taps[i]);
+                volk_free(taps);
+            }
+
+            // Init buffer
+            buffer.resize(8192);
+            buffer_size = buffer.size();
+            in_buffer = 0;
+
+            // Get alignement parameters
+            align = volk_get_alignment();
+            aligned_tap_count = std::max<int>(1, align / sizeof(T));
+
+            // Set taps
+            ntaps = p_taps.size();
+
+            // Init taps
+            this->taps = (TT **)volk_malloc(aligned_tap_count * sizeof(TT *), align);
+            for (int i = 0; i < aligned_tap_count; i++)
+            {
+                this->taps[i] = (TT *)volk_malloc((ntaps + aligned_tap_count - 1) * sizeof(TT), align);
+                for (int y = 0; y < ntaps + aligned_tap_count - 1; y++)
+                    this->taps[i][y] = 0;
+                for (int j = 0; j < ntaps; j++)
+                    this->taps[i][i + j] = p_taps[(ntaps - 1) - j]; // Reverse taps
+            }
+        }
+
+        template <typename T, typename TT>
+        uint32_t DecimatingFIRBlock<T, TT>::process(T *input, uint32_t nsamples, T *output)
+        {
+            if (needs_reinit)
+            {
+                needs_reinit = false;
+                init();
+            }
+
+            // Automatically grow buffer
+            if (buffer_size < in_buffer + nsamples)
+            {
+                buffer.resize((buffer_size + nsamples) * 2);
+                buffer_size = buffer.size();
+            }
+
+            memcpy(&buffer[in_buffer], input, nsamples * sizeof(T));
+            in_buffer += nsamples;
+
+            int to_use = (int)in_buffer - ntaps;
+            if (to_use < 0)
+                to_use = 0;
+
+            int nout = 0;
+
+            if constexpr (std::is_same_v<T, float>)
+            {
+                for (int i = 0; i < to_use; i++)
+                {
+                    if (decim_pos == decimation)
+                    {
+                        // Doing it this way instead of the normal :
+                        // volk_32f_x2_dot_prod_32f(&output_stream->writeBuf[i], &buffer[i + 1], taps, ntaps);
+                        // turns out to enhance performances quite a bit... Initially tested this after noticing
+                        // inconsistencies between the above simple way and GNU Radio's implementation
+                        const float *ar = (float *)((size_t)&buffer[i + 1] & ~(align - 1));
+                        const unsigned al = &buffer[i + 1] - ar;
+                        volk_32f_x2_dot_prod_32f_a(&output[nout++], ar, taps[al], ntaps + al);
+                        decim_pos = 1;
+                    }
+                    decim_pos++;
+                }
+            }
+            if constexpr (std::is_same_v<T, complex_t>)
+            {
+                if constexpr (std::is_same_v<TT, float>)
+                {
+                    for (int i = 0; i < to_use; i++)
+                    {
+                        if (decim_pos == decimation)
+                        {
+                            // Doing it this way instead of the normal :
+                            // volk_32fc_32f_dot_prod_32fc(&output_stream->writeBuf[i], &buffer[i + 1], taps, ntaps);
+                            // turns out to enhance performances quite a bit... Initially tested this after noticing
+                            // inconsistencies between the above simple way and GNU Radio's implementation
+                            const complex_t *ar = (complex_t *)((size_t)&buffer[i + 1] & ~(align - 1));
+                            const unsigned al = &buffer[i + 1] - ar;
+                            volk_32fc_32f_dot_prod_32fc_a((lv_32fc_t *)&output[nout++], (lv_32fc_t *)ar, taps[al], ntaps + al);
+                            decim_pos = 1;
+                        }
+                        decim_pos++;
+                    }
+                }
+                if constexpr (std::is_same_v<TT, complex_t>)
+                {
+                    for (int i = 0; i < to_use; i++)
+                    {
+                        if (decim_pos == decimation)
+                        {
+                            // Doing it this way instead of the normal :
+                            // volk_32fc_32f_dot_prod_32fc(&output_stream->writeBuf[i], &buffer[i + 1], taps, ntaps);
+                            // turns out to enhance performances quite a bit... Initially tested this after noticing
+                            // inconsistencies between the above simple way and GNU Radio's implementation
+                            const complex_t *ar = (complex_t *)((size_t)&buffer[i + 1] & ~(align - 1));
+                            const unsigned al = &buffer[i + 1] - ar;
+                            volk_32fc_x2_dot_prod_32fc_a((lv_32fc_t *)&output[nout++], (lv_32fc_t *)ar, (lv_32fc_t *)taps[al], ntaps + al);
+                            decim_pos = 1;
+                        }
+                        decim_pos++;
+                    }
+                }
+            }
+
+            memmove(&buffer[0], &buffer[to_use], (in_buffer - to_use) * sizeof(T));
+            in_buffer -= to_use;
+
+            return nout;
+        }
+
+        template class DecimatingFIRBlock<complex_t, complex_t>;
+        template class DecimatingFIRBlock<complex_t, float>;
+        template class DecimatingFIRBlock<float, float>;
+    } // namespace ndsp
+} // namespace satdump
---
+++ src-core/dsp/filter/decimating_fir.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "common/dsp/block.h"
+#include "common/dsp/complex.h"
+#include "dsp/block.h"
+#include "dsp/block_simple.h"
+#include "dsp/complex_json.h"
+#include <volk/volk_alloc.hh>
+
+namespace satdump
+{
+    namespace ndsp
+    {
+        template <typename T, typename TT = float>
+        class DecimatingFIRBlock : public BlockSimple<T, T>
+        {
+        public:
+            bool needs_reinit = false;
+            std::vector<TT> p_taps;
+
+        private:
+            int buffer_size = 0;
+            volk::vector<T> buffer;
+            TT **taps = nullptr;
+            int ntaps;
+            int align;
+            int aligned_tap_count;
+
+            size_t in_buffer;
+
+            int decimation = 1;
+            int decim_pos = 1;
+
+        public:
+            uint32_t process(T *input, uint32_t nsamples, T *output);
+
+        public:
+            DecimatingFIRBlock(std::string id = "");
+            ~DecimatingFIRBlock();
+
+            void init();
+
+            nlohmann::json get_cfg(std::string key)
+            {
+                if (key == "taps")
+                    return p_taps;
+                else if (key == "decimation")
+                    return decimation;
+                else
+                    throw satdump_exception(key);
+            }
+
+            Block::cfg_res_t set_cfg(std::string key, nlohmann::json v)
+            {
+                if (key == "taps")
+                {
+                    p_taps = v.get<std::vector<TT>>();
+                    needs_reinit = true;
+                }
+                else if (key == "decimation")
+                {
+                    decimation = v;
+                    if (decimation < 1)
+                        decimation = 1;
+                }
+                else
+                    throw satdump_exception(key);
+                return Block::RES_OK;
+            }
+        };
+    } // namespace ndsp
+} // namespace satdump
