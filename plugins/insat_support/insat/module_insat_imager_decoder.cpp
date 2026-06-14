f54726fd4e52af05ed29a4a91e1efdaa213c55ac
Aang23 <qwerty15@gmx.fr>
2026-06-14 11:21:23 +0200

    Insat stuff

---
+++ plugins/insat_support/insat/module_insat_imager_decoder.cpp
@@ -0,0 +1,147 @@
+#include "module_insat_imager_decoder.h"
+#include "common/codings/differential/generic.h"
+#include "common/codings/differential/nrzm.h"
+#include "common/codings/types.h"
+#include "common/utils.h"
+#include "common/widgets/themed_widgets.h"
+#include "imgui/imgui.h"
+#include "logger.h"
+#include <cstdint>
+
+#define BUFFER_SIZE 8192
+
+namespace insat
+{
+    INSATImagerDecoderModule::INSATImagerDecoderModule(std::string input_file, std::string output_file_hint, nlohmann::json parameters)
+        : satdump::pipeline::base::FileStreamToFileStreamModule(input_file, output_file_hint, parameters), //
+          viterbi(0.25, 200, BUFFER_SIZE, {PHASE_0, PHASE_90}, false),                                     //
+          def(0x0eb8cec1, 32, 4320000, 0, false, true)
+    {
+        frame_cnt = 0;
+        fsfsm_file_ext = ".frm";
+    }
+
+    INSATImagerDecoderModule::~INSATImagerDecoderModule() {}
+
+    void INSATImagerDecoderModule::process()
+    {
+        // Buffers
+        int8_t buffer[BUFFER_SIZE];
+        uint8_t vito_buffer[BUFFER_SIZE];
+
+        // Diff dec
+        diff::NRZMDiff diff;
+
+        // Randomization sequence
+        uint8_t rand_seq[1023] = {
+            0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1,
+            0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+            0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+            1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1,
+            1, 1, 1, 1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1,
+            1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1,
+            0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1,
+            0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1,
+            1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1,
+            0, 0, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1,
+            0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1,
+            0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1,
+            0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1,
+            1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1,
+            1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0};
+
+        uint8_t rand_seq_b[540000];
+        for (int i = 0; i < 4320000; i++)
+            rand_seq_b[i / 8] = rand_seq_b[i / 8] << 1 | rand_seq[(i + 353) % 1023];
+
+        while (should_run())
+        {
+            // Read buffer
+            read_data((uint8_t *)buffer, BUFFER_SIZE);
+
+            int vitn = viterbi.work(buffer, BUFFER_SIZE, vito_buffer);
+
+            diff.decode_bits(vito_buffer, vitn);
+
+            auto frames = def.work(vito_buffer, vitn / 8);
+
+            for (auto &frame : frames)
+            {
+                for (int i = 4; i < 540000; i++)
+                    frame[i] ^= rand_seq_b[i];
+
+                write_data((uint8_t *)frame.data(), 540000);
+
+                frame_cnt++;
+            }
+        }
+
+        logger->critical("DONE");
+
+        cleanup();
+    }
+
+    nlohmann::json INSATImagerDecoderModule::getModuleStats()
+    {
+        auto v = satdump::pipeline::base::FileStreamToFileStreamModule::getModuleStats();
+        v["viterbi_lock"] = viterbi.getState();
+        v["viterbi_ber"] = viterbi.ber();
+        v["frame_cnt"] = frame_cnt;
+        return v;
+    }
+
+    void INSATImagerDecoderModule::drawUI(bool window)
+    {
+        ImGui::Begin("INSAT Imager Decoder", NULL, window ? 0 : NOWINDOW_FLAGS);
+
+        ImGui::BeginGroup();
+        {
+            int viterbi_lock = viterbi.getState();
+            float ber = viterbi.ber();
+
+            ImGui::Button("Viterbi", {200 * ui_scale, 20 * ui_scale});
+            {
+                ImGui::Text("State : ");
+
+                ImGui::SameLine();
+
+                if (viterbi.getState() == 0)
+                    ImGui::TextColored(style::theme.red, "NOSYNC");
+                else
+                    ImGui::TextColored(style::theme.green, "SYNCED");
+
+                ImGui::Text("BER   : ");
+                ImGui::SameLine();
+                ImGui::TextColored(viterbi_lock == 0 ? style::theme.red : style::theme.green, UITO_C_STR(ber));
+
+                std::memmove(&ber_history[0], &ber_history[1], (200 - 1) * sizeof(float));
+                ber_history[200 - 1] = ber;
+
+                satdump::widgets::ThemedPlotLines(style::theme.plot_bg.Value, "##", ber_history, IM_ARRAYSIZE(ber_history), 0, "", 0.0f, 1.0f, ImVec2(200 * ui_scale, 50 * ui_scale));
+            }
+
+            ImGui::Spacing();
+
+            ImGui::Button("Deframer", {200 * ui_scale, 20 * ui_scale});
+            {
+                ImGui::Text("Frames : %d", frame_cnt);
+
+                ImGui::SameLine();
+            }
+        }
+        ImGui::EndGroup();
+
+        drawProgressBar();
+
+        ImGui::End();
+    }
+
+    std::string INSATImagerDecoderModule::getID() { return "insat_imager_decoder"; }
+
+    std::shared_ptr<satdump::pipeline::ProcessingModule> INSATImagerDecoderModule::getInstance(std::string input_file, std::string output_file_hint, nlohmann::json parameters)
+    {
+        return std::make_shared<INSATImagerDecoderModule>(input_file, output_file_hint, parameters);
+    }
+} // namespace insat
