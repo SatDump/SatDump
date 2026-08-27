bc2d539df6d5551d3af85963e5e05e29f83ed15a
RAD750 <jacopo@a-centauri.com>
2026-08-27 22:21:07 +0200

    Update Italian localisation

---
+++ src-core/satdump_varst.cpp
@@ -0,0 +1,10 @@
+#define SATDUMP_DLL_EXPORT 1
+#include "satdump_vars.h"
+
+namespace satdump
+{
+    SATDUMP_DLL const std::string RESOURCES_PATH = "/usr/share/satdump/";
+    SATDUMP_DLL const std::string LIBRARIES_PATH = "/usr/local/lib/satdump/";
+    SATDUMP_DLL const std::string SATDUMP_VERSION = "2.0.0-alpha-1d199f863";
+    SATDUMP_DLL const std::string SATDUMP_VERSION_TAG = "dev/";
+} // namespace satdump
