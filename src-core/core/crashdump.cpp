6561b61b3acf5cfb868fdda3fab6b78103701bfc
Aang23 <qwerty15@gmx.fr>
2026-09-20 18:03:16 +0200

    Is it easter already?

--- src-core/core/crashdump.cpp
+++ src-core/core/crashdump.cpp
@@ -32,6 +32,49 @@ namespace satdump
     {
         std::string report;
 
+        if (time(NULL) % 5 == 0)
+        {
+            report += "                  KI                |-------------------------------|      \n";
+            report += "                KING                | SPECIAL MESSAGE FROM THE KING |      \n";
+            report += "               NOAA15               |-------------------------------|      \n";
+            report += "            NOAA15 KIN                                                        \n";
+            report += "           NOAA15 N         AAN                                                \n";
+            report += "          NOAA15-AAN       KINGN                                               \n";
+            report += "         NOAA15 NOAA15N   N  AAN                                               \n";
+            report += "        NOAA15 NOAA15KINGN KINGN                               KING              \n";
+            report += "       NOAA15-NOAA15NOAA15AAN AN                              NOAA15KING      \n";
+            report += "       NOAA15 NOAA15N/--\5NOAA15N                           NOAA15KING        \n";
+            report += "      NOAA15 NOAA15NO|  |NOAA15KING  AAN                  ----------          \n";
+            report += "      NOAA15-KINGNOAA\\--/1/--\5KINGNOAA15KI             NOAA15KING            \n";
+            report += "     NOAA1        NOAA15KI|  |NGNOAA15KINGN          NOAA15KING              \n";
+            report += "                   NOAA15K\\--/5N/--\5NOAA15        ----------                \n";
+            report += "                   N NOAA15NOAA1|  |A15KING######NOAA15KING                  \n";
+            report += "                   N  AAN  NOAA1\\--/A15KING###NOAA15NOAA15                    \n";
+            report += "                 -/N\\-    NOAA15NOAA15KINGN  -----------                      \n";
+            report += "                 ||N||       N  NOAA15AAN  NOAA15KING                        \n";
+            report += "                 ||N||            NOAA15 -----------                          \n";
+            report += "                                    N  NOAA15KING                            \n";
+            report += "                                     NOAA15KING                              \n";
+            report += "                                   -----------                                \n";
+            report += "                                  NOAA15KING                                  \n";
+            report += "                                NOAA15KING                                     \n";
+            report += "                                                                              \n";
+            report += "|------------------------------------------------------------------|\n";
+            report += "|SUBJECT: CONCERNS ABOUT YOUR LACK OF WORSHIPMENT TO HIS MAJESTY   |\n";
+            report += "|------------------------------------------------------------------|\n";
+            report += "|Dear SatDump user, it has come to my attention that you didn't pay|\n";
+            report += "|the homages I was expecting. I have therefore decided to make your|\n";
+            report += "|SatDump copy crash.                                               |\n";
+            report += "|You may copy this crash report to the developers and they might be|\n";
+            report += "|able to figure something out for you.                             |\n";
+            report += "|                                                                  |\n";
+            report += "|Glory to the eternal POES kingdom.                                |\n";
+            report += "|                                                                  |\n";
+            report += "|HIS MAJESTY THE KING NOAA 15                                      |\n";
+            report += "|------------------------------------------------------------------|\n";
+        }
+
+        report += "\n";
         report += "   ______                __    ____                      \n";
         report += "  / ____/________ ______/ /_  / __ \\__  ______ ___  ____ \n";
         report += " / /   / ___/ __ `/ ___/ __ \\/ / / / / / / __ `__ \\/ __ \\\n";
