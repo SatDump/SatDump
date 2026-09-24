722e65ef457814f1367c55d97495d54836b1d327
Meti172 <100243410+Meti172@users.noreply.github.com>
2026-09-24 20:45:11 +0200

    Confused data types in docs.. oops

--- plugins/fengyun2_support/fengyun2/svissr/module_svissr_image_decoder.cpp
+++ plugins/fengyun2_support/fengyun2/svissr/module_svissr_image_decoder.cpp
@@ -479,7 +479,7 @@ namespace fengyun_svissr
             if (save_telemetry)
             {
                 logger->debug("Processing satellite telemetry...");
-                std::string active_sensor = (Calib_2_block[10] >> 7) == 1 ? "Primary" : "Backup";
+                std::string active_sensor = (Calib_2_block[10] & 0b1) == 1 ? "Primary" : "Backup";
 
                 uint8_t active_detectors = satdump::majority_law_vec(detector_telemetry_stats)[0];
                 std::string telemetry_data = SVISSRTelemetryDumper::dump_telemetry_to_JSON(*orbit_attitude_block, attitude_prediction_data, orbit_prediction_data, active_sensor, active_detectors);
